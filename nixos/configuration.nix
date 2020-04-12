47921421b352db7264b37b85a0310e2682683a36
Even Brenden <evenbrenden@gmail.com>
2020-04-12 21:12:37 +0200

    Move all things Nix to this repo and restructure

---
+++ nixos/configuration.nix
@@ -0,0 +1,133 @@
+{ config, pkgs, ... }:
+
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/sda3";
+      preLVM = true;
+    }
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+  boot.initrd.checkJournalingFS = false;
+
+  networking.hostName = "naxos";
+  networking.networkmanager.enable = true;
+
+  security.rngd.enable = false;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  nixpkgs.config.pulseaudio = true; # https://nixos.wiki/wiki/Xfce
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      arandr
+      autorandr
+      cabal-install
+      chromium
+      curl
+      dbeaver
+      dos2unix
+      drive
+      firefox
+      gimp
+      git
+      irssi
+      jetbrains.rider
+      networkmanagerapplet
+      nomacs
+      libsForQt5.vlc
+      linuxConsoleTools
+      postman
+      python3
+      python37Packages.virtualenv
+      remmina
+      sakura
+      slack
+      spotify
+      unstable.dotnet-sdk_3
+      unstable.teams
+      unzip
+      veracrypt
+      virtualbox
+      # virtualboxExtpack
+      vscode
+      xorg.xdpyinfo
+      linuxPackages.virtualboxGuestAdditions
+    ];
+    pathsToLink = [ "/libexec" ]; # For i3
+  };
+
+  programs.ssh.startAgent = true;
+
+  nix.gc.automatic = true;
+  nix.gc.dates = "04:00";
+
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "Europe/Amsterdam";
+
+  users.users.evenbrenden = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "network-manager" "vboxusers" ];
+  };
+
+  virtualisation.virtualbox.guest = {
+    enable = true;
+    x11 = true;
+  };
+
+  services.xserver = {
+    enable = true;
+    layout = "us,no";
+    libinput.enable = true;
+    libinput.tapping = true;
+    displayManager = {
+      slim = {
+        enable = true;
+        defaultUser = "evenbrenden";
+      };
+    };
+    windowManager.i3.enable = true;
+    desktopManager = {
+      default = "xfce";
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+  };
+
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "no";
+  services.haveged.enable = true;
+
+  system.stateVersion = "19.09";
+}
---
+++ nixpkgs/home.nix
@@ -0,0 +1,57 @@
+{ config, pkgs, ... }:
+
+let
+  vim-fsharp = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-fsharp";
+    src = pkgs.fetchFromGitHub {
+      owner = "wsdjeg";
+      repo = "vim-fsharp";
+      rev = "a4255ba4866fa5aba91fec342f98964cffbbc542";
+      sha256 = "0vlr90x4rp30a98k0g1g0fmwmp0slblp74fr8zaclyvbc8kwzimc";
+    };
+  };
+  vim-pico8-syntax = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-pico-syntax";
+    src = pkgs.fetchFromGitHub {
+      owner = "justinj";
+      repo = "vim-pico8-syntax";
+      rev = "dbdd92fad0533eeaeaea844815d4de11e9507ce7";
+      sha256 = "0say0bb74rdbabgsf7rrbm8x841pmgh80fwr6kn94fgphr3vhm0s";
+    };
+  };
+in
+{
+  programs = {
+    home-manager.enable = true;
+    man.enable = false;
+    vim = with pkgs.vimPlugins; {
+      enable = true;
+      extraConfig = pkgs.lib.strings.fileContents ./dotfiles/vimrc;
+      plugins = [
+        vim-airline
+        vim-gitgutter
+        tcomment_vim
+        vim-nix
+        vim-fsharp
+        vim-pico8-syntax
+      ];
+    };
+  };
+  xdg = {
+    enable = true;
+    configFile = {
+      "sakura/sakura.conf".source = ./dotfiles/sakura.conf;
+      "i3/config".source = ./dotfiles/i3config;
+      "i3status/config".source = ./dotfiles/i3status;
+    };
+  };
+  home.file = {
+    ".bashrc".source = ./dotfiles/bashrc;
+    ".gitignore".source = ./dotfiles/gitignore;
+    ".gitconfig".source = ./dotfiles/gitconfig;
+    ".driverc".source = ./dotfiles/driverc;
+    "toggle_keyboard_layout.py".source = ./dotfiles/toggle_keyboard_layout.py;
+    "adjust_brightness.py".source = ./dotfiles/adjust_brightness.py;
+    "notifications.py".source = ./dotfiles/notifications.py;
+  };
+}
