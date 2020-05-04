8bcbc15da2b0a779601d6ad6e5c7e57238f1ad1d
Even Brenden <evenbrenden@gmail.com>
2020-05-04 12:38:35 +0200

    Remove default locale and console font
    The former is already the default and
    the latter is overridden in editor.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -94,8 +94,8 @@ in
   nix.gc.automatic = true;
   nix.gc.dates = "04:00";
 
-  console.font = "Lat2-Terminus16";
-  i18n.defaultLocale = "en_US.UTF-8";
+#  console.font = "Lat2-Terminus16";
+#  i18n.defaultLocale = "en_US.UTF-8";
 
   time.timeZone = "Europe/Amsterdam";
 
