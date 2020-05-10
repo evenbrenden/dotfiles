919172159cac31ba1182fa760bca618c0f037f81
Even Brenden <evenbrenden@gmail.com>
2020-05-10 11:02:55 +0200

    Tidy up

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -130,15 +130,13 @@ in
       headphonesIn = {
         event = "jack/headphone HEADPHONE plug";
         action = ''
-            export PATH="$PATH:/run/current-system/sw/bin/"
-            amixer -c 0 set 'Bass Speaker' off
+            /run/current-system/sw/bin/amixer -c 0 set 'Bass Speaker' off
         '';
       };
       headphonesOut = {
         event = "jack/headphone HEADPHONE unplug";
         action = ''
-            export PATH="$PATH:/run/current-system/sw/bin/"
-            amixer -c 0 set 'Bass Speaker' on
+            /run/current-system/sw/bin/amixer -c 0 set 'Bass Speaker' on
         '';
       };
     };
