1f230115c4cef14353e0b738588736ded19425e3
Even Brenden <evenbrenden@gmail.com>
2020-05-18 17:04:47 +0200

    NixOS: Increase inotify watches limit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,6 +25,9 @@ in
       };
       efi.canTouchEfiVariables = true;
     };
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = 524288;
+    };
     kernelPackages = pkgs.linuxPackages_latest;
   };
 
