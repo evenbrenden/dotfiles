20829ddbe28e4d54e77e65a96d13151ba1dbb674
Even Brenden <evenbrenden@gmail.com>
2020-05-21 12:00:16 +0200

    i3: Squash toggle script
    Could squash X1C7 and T490s altogether,
    leaving them separated for now.

--- nixpkgs/dotfiles/x1c7_toggle_mute.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-import subprocess
-subprocess.run(['pactl', 'set-sink-mute', '@DEFAULT_SINK@', 'toggle'], stdout=subprocess.PIPE)
-subprocess.run(['pkill', '-x', '-USR1', 'i3status'], stdout=subprocess.PIPE)
