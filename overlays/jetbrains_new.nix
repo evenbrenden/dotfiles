bc55b7c20fd122361210a06de444864f35a74292
Even Brenden <evenbrenden@gmail.com>
2020-06-02 10:13:33 +0200

    Refactor Rider overlays

--- overlays/jetbrains_new.nix
+++ overlays/jetbrains_new.nix
@@ -1,12 +1,9 @@
-let
-  version = "2020.1.3";
-in
 (self: super:
   {
     jetbrains = super.jetbrains // {
-      rider = super.jetbrains.rider.overrideDerivation (_: {
+      rider = super.jetbrains.rider.overrideDerivation (_: rec {
         name = "rider-${version}";
-        version = "${version}";
+        version = "2020.1.3";
 
         src = super.fetchurl {
           url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
