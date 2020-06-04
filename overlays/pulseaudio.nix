ca863203aa253504a8face816a37575dc1bf5820
Even Brenden <evenbrenden@gmail.com>
2020-06-04 11:13:20 +0200

    Do the pulseaudio overlay the right way

--- overlays/pulseaudio.nix
+++ overlays/pulseaudio.nix
@@ -1,15 +1,26 @@
 (self: super:
   {
-    pulseaudio = super.pulseaudio.overrideAttrs (_: rec {
-      name = builtins.replaceStrings [super.pulseaudio.version] [version] super.pulseaudio.name;
-      version = "13.99";
+    pulseaudio = super.callPackage
+      ({ libOnly ? false }:
 
-      src = super.fetchurl {
-        url = "http://freedesktop.org/software/pulseaudio/releases/pulseaudio-${version}.1.tar.xz";
-        sha256 = "030a7v0khp6w683km81c6vpch1687pvx2gvscnzkjq4f0z6138g6";
-      };
+      super.pulseaudio.overrideAttrs (_: rec {
+        name = "${if libOnly then "lib" else ""}pulseaudio-${version}";
+        version = "13.99";
 
-      postInstall = super.pulseaudio.postInstall;
-    });
+        src = super.fetchurl {
+          url = "http://freedesktop.org/software/pulseaudio/releases/pulseaudio-${version}.1.tar.xz";
+          sha256 = "030a7v0khp6w683km81c6vpch1687pvx2gvscnzkjq4f0z6138g6";
+        };
+
+        postInstall = super.lib.optionalString libOnly ''
+          rm -rf $out/{bin,share,etc,lib/{pulse-*,systemd}}
+          sed 's|-lltdl|-L${super.libtool.lib}/lib -lltdl|' -i $out/lib/pulseaudio/libpulsecore-${version}.la
+        ''
+          + ''
+          moveToOutput lib/cmake "$dev"
+          rm -f $out/bin/qpaeq # this is packaged by the "qpaeq" package now, because of missing deps
+        '';
+      }))
+    {};
   }
 )
