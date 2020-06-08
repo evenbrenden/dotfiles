3ab463cc4c8b299bf6957a1c41305af2ae7687df
Even Brenden <evenbrenden@gmail.com>
2020-06-08 21:05:31 +0200

    NixOS: Use sof-firmware v1.5
    Need to inline go.sh from v1.4.2 until the v1.5 source is updated.

--- overlays/sof-firmware.nix
+++ overlays/sof-firmware.nix
@@ -2,17 +2,48 @@
   {
     sof-firmware = super.sof-firmware.overrideAttrs (_: rec {
       pname = "sof-firmware";
-      version = "1.4.2";
+      version = "1.5";
 
       src = super.fetchzip {
         url = "https://github.com/thesofproject/sof-bin/archive/stable-v${version}.zip";
-        sha256 = "0djkqb8lgyql78pv9mcnda0i3lkjk1whk9ai5gzrz1p68dyppjc9";
+        sha256 = "11vs7ncsysj77j6s9dskyxbbx92kz9xf003p5gdxxql5l9m7fqkw";
       };
 
       installPhase = ''
         mkdir -p $out/lib/firmware/intel
-        substituteInPlace go.sh --replace 'ROOT=' 'ROOT=$out'
-        ./go.sh
+
+        ROOT=$out
+        INTEL_PATH=lib/firmware/intel
+        VERSION=v${version}
+
+        echo "Installing Intel firmware and topology $VERSION to $INTEL_PATH"
+
+        # wipe previous releases
+        rm -rf ''${ROOT}/''${INTEL_PATH}/sof/*
+        rm -rf ''${ROOT}/''${INTEL_PATH}/sof-tplg-*
+        rm -rf ''${ROOT}/''${INTEL_PATH}/sof-tplg
+
+        # copy to destination
+        cd lib/firmware
+        cp -rf intel ''${ROOT}/lib/firmware
+
+        # add symlinks
+        cd ''${ROOT}/''${INTEL_PATH}/sof
+
+        ln -s ''${VERSION}/sof-bdw-''${VERSION}.ri sof-bdw.ri
+        ln -s ''${VERSION}/sof-byt-''${VERSION}.ri sof-byt.ri
+        ln -s ''${VERSION}/sof-cht-''${VERSION}.ri sof-cht.ri
+        ln -s ''${VERSION}/intel-signed/sof-apl-''${VERSION}.ri sof-apl.ri
+        ln -s ''${VERSION}/intel-signed/sof-apl-''${VERSION}.ri sof-glk.ri
+        ln -s ''${VERSION}/intel-signed/sof-cnl-''${VERSION}.ri sof-cfl.ri
+        ln -s ''${VERSION}/intel-signed/sof-cnl-''${VERSION}.ri sof-cnl.ri
+        ln -s ''${VERSION}/intel-signed/sof-cnl-''${VERSION}.ri sof-cml.ri
+        ln -s ''${VERSION}/intel-signed/sof-icl-''${VERSION}.ri sof-icl.ri
+
+        cd ..
+        ln -s sof-tplg-''${VERSION} sof-tplg
+
+        echo "Done installing Intel firmware and topology ''$VERSION"
       '';
     });
   }
