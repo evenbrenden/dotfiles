e3508f156245a025ae71f56ed7548089ef322f1c
Even Brenden <evenbrenden@gmail.com>
2020-07-18 13:27:43 +0200

    Restructure

--- home_install.sh
+++ home_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf ~/.config/nixpkgs \
-    && cp -r nixpkgs ~/.config/ \
+    && cp -r home ~/.config/nixpkgs \
     && home-manager switch \
     && i3-msg reload \
     && i3-msg restart
