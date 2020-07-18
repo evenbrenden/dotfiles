2cb172b252c24591717b13d7ab25caae93838a37
Even Brenden <evenbrenden@gmail.com>
2020-07-18 13:59:41 +0200

    Disable systemd sleep et al.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -162,5 +162,10 @@
     };
   };
 
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
   system.stateVersion = "20.03";
 }
