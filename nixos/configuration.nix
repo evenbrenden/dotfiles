591a0dd7ed2fdcfcac321b40df2d538fd57707b4
Even Brenden <evenbrenden@gmail.com>
2020-07-19 11:45:15 +0200

    Remove XFCE
    Also removes xfce4-power-manager.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -153,14 +153,6 @@
         '';
       };
       windowManager.i3.enable = true;
-      desktopManager = {
-        xterm.enable = false;
-        xfce = {
-          enable = true;
-          noDesktop = true;
-          enableXfwm = false;
-        };
-      };
     };
   };
 
