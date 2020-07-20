02d457636ac2f6725e81999af75cbf2c96c7cae0
Even Brenden <evenbrenden@gmail.com>
2020-07-20 23:44:44 +0200

    Change some PM settings

--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -3,22 +3,17 @@
 # No battery actions or notifications (i3status is sufficient)
 # No lid actions (screen shuts off on lid close anyway)
 # No display power management (manual brightness control)
-# Lock and sleep on inactivity (all power buttons ignored)
+# Lock inactivity (and sleep on i3wm keyboard binding)
 
 let
-  sleep_time = 30; # minutes
   lock_time = 15; # minutes
   lock_notify = 30; # seconds
 in
   {
     environment.systemPackages = [ pkgs.brightnessctl ];
+
     services = {
       logind = {
-        # This doesn't seem to work
-        extraConfig = ''
-          IdleAction=hybrid-sleep
-          IdleActionSec=${toString sleep_time}min
-        '';
         lidSwitch = "ignore";
         lidSwitchDocked = "ignore";
         lidSwitchExternalPower = "ignore";
@@ -38,6 +33,7 @@ in
         };
       };
     };
+
     services.tlp.enable = true;
     boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call ]; # For ThinkPads x TLP
   }
