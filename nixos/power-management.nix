19a008f289aae9297141cb2992b8972867d104fe
Even Brenden <evenbrenden@gmail.com>
2020-07-22 10:27:49 +0200

    PM: Lock after 60 minutes of inactivity
    Also revise comments.

--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -1,20 +1,20 @@
 { config, pkgs, ... }:
 
-# No battery actions or notifications (i3status is sufficient)
-# No lid actions (screen shuts off on lid close anyway)
-# No display power management (manual brightness control)
-# Lock on inactivity (using i3lock directly) (for security)
-# Make sure screen is locked on sleep (using xss-lock)
-# Sleep and shutdown on command only (using i3 key binds)
-# Let TLP do its thing (whatever it is) in the background
+# No battery actions or notifications (status bar tells all)
+# No lid actions (screen powers off when lid is closed)
+# No screen saver or DPMS (manual brightness control)
+# No inactivity actions (except lock for security)
+# Lock on sleep (thanx xss-lock)
+# Let TLP do its thing (whatever it is)
 
 let
-  lock_time = 15; # minutes
-  lock_notify = 30; # seconds
+  lock_time = 60; # minutes
+  lock_notify = 60; # seconds
 in
   {
     environment.systemPackages = [ pkgs.brightnessctl ];
 
+    # nixpkgs issue #67350 (only need this for lock on sleep though)
     programs.xss-lock = {
       enable = true;
       lockerCommand = ''${pkgs.i3lock}/bin/i3lock --color 000000'';
@@ -31,12 +31,13 @@ in
           xset s off
           xset -dpms
         '';
+        # can be disabled and reenabled with systemctl
         xautolock = {
           enable = true;
           enableNotifier = true;
           extraOptions = [ "-secure" ];
           locker = ''${pkgs.i3lock}/bin/i3lock --color 000000'';
-          notifier = ''${pkgs.libnotify}/bin/notify-send "Locking in ${toString lock_notify} seconds"'';
+          notifier = ''${pkgs.libnotify}/bin/notify-send "Lock in ${toString lock_notify} seconds"'';
           notify = lock_notify;
           time = lock_time;
         };
