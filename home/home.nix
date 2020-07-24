6259ba02b0cb13a4d9ed10aebbfccdec2fa81c87
Even Brenden <evenbrenden@gmail.com>
2020-07-24 11:30:28 +0200

    Tidy up

--- home/home.nix
+++ home/home.nix
@@ -11,41 +11,42 @@ let
     };
   };
 in
-{
-  programs = {
-    home-manager.enable = true;
-    man.enable = false;
-    neovim = with pkgs.vimPlugins; {
+  {
+    programs = {
+      home-manager.enable = true;
+      man.enable = false;
+      neovim = with pkgs.vimPlugins; {
+        enable = true;
+        extraConfig = pkgs.lib.strings.fileContents ./dotfiles/init.vim;
+        plugins = [
+          vim-airline
+          vim-fsharp
+          vim-gitgutter
+          vim-markdown
+          vim-nix
+          vim-pico8-syntax
+          tcomment_vim
+        ];
+      };
+    };
+    xdg = {
       enable = true;
-      extraConfig = pkgs.lib.strings.fileContents ./dotfiles/init.vim;
-      plugins = [
-        vim-airline
-        vim-fsharp
-        vim-gitgutter
-        vim-markdown
-        vim-nix
-        vim-pico8-syntax
-        tcomment_vim
-      ];
+      configFile = {
+        "sakura/sakura.conf".source = ./dotfiles/sakura.conf;
+        "i3/config".source = ./dotfiles/i3config;
+        "i3status/config".source = ./dotfiles/i3status;
+        "dunst/dunstrc".source = ./dotfiles/dunstrc;
+        "autorandr".source = ./dotfiles/autorandr;
+      };
     };
-  };
-  xdg = {
-    enable = true;
-    configFile = {
-      "sakura/sakura.conf".source = ./dotfiles/sakura.conf;
-      "i3/config".source = ./dotfiles/i3config;
-      "i3status/config".source = ./dotfiles/i3status;
-      "dunst/dunstrc".source = ./dotfiles/dunstrc;
-      "autorandr".source = ./dotfiles/autorandr;
+    home.file = {
+      ".bashrc".source = ./dotfiles/bashrc;
+      ".gitignore".source = ./dotfiles/gitignore;
+      ".gitconfig".source = ./dotfiles/gitconfig;
+      "bin/adjust_brightness.py".source = ./dotfiles/adjust_brightness.py;
+      "bin/notifications.py".source = ./dotfiles/notifications.py;
+      "bin/toggle_keyboard_layout.py".source = ./dotfiles/toggle_keyboard_layout.py;
+      "bin/i3status.sh".source = ./dotfiles/i3status.sh;
     };
-  };
-  home.file = {
-    ".bashrc".source = ./dotfiles/bashrc;
-    ".gitignore".source = ./dotfiles/gitignore;
-    ".gitconfig".source = ./dotfiles/gitconfig;
-    "bin/adjust_brightness.py".source = ./dotfiles/adjust_brightness.py;
-    "bin/notifications.py".source = ./dotfiles/notifications.py;
-    "bin/toggle_keyboard_layout.py".source = ./dotfiles/toggle_keyboard_layout.py;
-    "bin/i3status.sh".source = ./dotfiles/i3status.sh;
-  };
-}
+  }
+
