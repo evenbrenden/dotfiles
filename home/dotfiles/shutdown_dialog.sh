28bab8d75e5c190e1235bbe69587ab63e3599089
Even Brenden <evenbrenden@gmail.com>
2020-07-24 10:10:17 +0200

    i3config: Change lock and sleep key binds + rm shutdown

--- home/dotfiles/shutdown_dialog.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-while [ "$select" != "No" -a "$select" != "Yes" ]; do
-    select=$(echo -e 'No\nYes' | dmenu -i -p "Shut down?")
-    [ -z "$select" ] && exit 0
-done
-[ "$select" = "No" ] && exit 0
-i3-msg exec shutdown now
