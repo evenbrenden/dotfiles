0172b07bdbd7c48f28020996d98715fb099941c1
Even Brenden <evenbrenden@gmail.com>
2020-07-28 10:36:06 +0200

    Move locking back to OS config

--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -3,16 +3,37 @@
 {
   environment.systemPackages = [ pkgs.brightnessctl ];
 
+  programs.xss-lock = {
+    enable = true;
+    lockerCommand = ''${pkgs.i3lock}/bin/i3lock --color 000000'';
+  };
+
   services = {
     logind = {
       lidSwitch = "ignore";
       lidSwitchDocked = "ignore";
       lidSwitchExternalPower = "ignore";
     };
-    xserver.displayManager.sessionCommands = ''
-      xset s off
-      xset -dpms
-    '';
+    xserver = {
+      displayManager.sessionCommands = ''
+        xset s off
+        xset -dpms
+      '';
+      xautolock =
+        let
+          lock-time = 60; # minutes
+          lock-notify = 60; # seconds
+        in
+          {
+            enable = true;
+            enableNotifier = true;
+            extraOptions = [ "-secure" ];
+            locker = ''${pkgs.systemd}/bin/loginctl lock-session $XDG_SESSION_ID'';
+            notifier = ''${pkgs.libnotify}/bin/notify-send "Lock in ${toString lock-notify} seconds"'';
+            notify = lock-notify;
+            time = lock-time;
+          };
+    };
   };
 
   services.tlp.enable = false;
