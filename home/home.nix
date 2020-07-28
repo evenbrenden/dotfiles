7f2caffefd5997a066324da3e9a4707dc7f780b8
Even Brenden <evenbrenden@gmail.com>
2020-07-28 10:36:47 +0200

    Fix indentation

--- home/home.nix
+++ home/home.nix
@@ -1,54 +1,54 @@
 { config, pkgs, ... }:
 
-  {
-    programs = {
-      home-manager.enable = true;
-      man.enable = false;
-      neovim = with pkgs.vimPlugins; {
-        enable = true;
-        extraConfig = pkgs.lib.strings.fileContents ./dotfiles/init.vim;
-        plugins =
-          let
-            vim-fsharp = pkgs.vimUtils.buildVimPlugin {
-              name = "vim-fsharp";
-              src = pkgs.fetchFromGitHub {
-                owner = "wsdjeg";
-                repo = "vim-fsharp";
-                rev = "a4255ba4866fa5aba91fec342f98964cffbbc542";
-                sha256 = "0vlr90x4rp30a98k0g1g0fmwmp0slblp74fr8zaclyvbc8kwzimc";
-              };
-            };
-          in
-            [
-              vim-airline
-              vim-fsharp
-              vim-gitgutter
-              vim-markdown
-              vim-nix
-              vim-pico8-syntax
-              tcomment_vim
-            ];
-      };
-    };
-
-    xdg = {
+{
+  programs = {
+    home-manager.enable = true;
+    man.enable = false;
+    neovim = with pkgs.vimPlugins; {
       enable = true;
-      configFile = {
-        "sakura/sakura.conf".source = ./dotfiles/sakura.conf;
-        "i3/config".source = ./dotfiles/i3config;
-        "i3status/config".source = ./dotfiles/i3status;
-        "dunst/dunstrc".source = ./dotfiles/dunstrc;
-        "autorandr".source = ./dotfiles/autorandr;
-      };
+      extraConfig = pkgs.lib.strings.fileContents ./dotfiles/init.vim;
+      plugins =
+        let
+          vim-fsharp = pkgs.vimUtils.buildVimPlugin {
+            name = "vim-fsharp";
+            src = pkgs.fetchFromGitHub {
+              owner = "wsdjeg";
+              repo = "vim-fsharp";
+              rev = "a4255ba4866fa5aba91fec342f98964cffbbc542";
+              sha256 = "0vlr90x4rp30a98k0g1g0fmwmp0slblp74fr8zaclyvbc8kwzimc";
+            };
+          };
+        in
+          [
+            vim-airline
+            vim-fsharp
+            vim-gitgutter
+            vim-markdown
+            vim-nix
+            vim-pico8-syntax
+            tcomment_vim
+          ];
     };
-    home.file = {
-      ".bashrc".source = ./dotfiles/bashrc;
-      ".gitignore".source = ./dotfiles/gitignore;
-      ".gitconfig".source = ./dotfiles/gitconfig;
-      "bin/adjust_brightness.py".source = ./dotfiles/adjust_brightness.py;
-      "bin/notifications.py".source = ./dotfiles/notifications.py;
-      "bin/toggle_keyboard_layout.py".source = ./dotfiles/toggle_keyboard_layout.py;
-      "bin/i3status.sh".source = ./dotfiles/i3status.sh;
+  };
+
+  xdg = {
+    enable = true;
+    configFile = {
+      "sakura/sakura.conf".source = ./dotfiles/sakura.conf;
+      "i3/config".source = ./dotfiles/i3config;
+      "i3status/config".source = ./dotfiles/i3status;
+      "dunst/dunstrc".source = ./dotfiles/dunstrc;
+      "autorandr".source = ./dotfiles/autorandr;
     };
-  }
+  };
+  home.file = {
+    ".bashrc".source = ./dotfiles/bashrc;
+    ".gitignore".source = ./dotfiles/gitignore;
+    ".gitconfig".source = ./dotfiles/gitconfig;
+    "bin/adjust_brightness.py".source = ./dotfiles/adjust_brightness.py;
+    "bin/notifications.py".source = ./dotfiles/notifications.py;
+    "bin/toggle_keyboard_layout.py".source = ./dotfiles/toggle_keyboard_layout.py;
+    "bin/i3status.sh".source = ./dotfiles/i3status.sh;
+  };
+}
 
