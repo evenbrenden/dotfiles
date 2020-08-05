f3630d0d70944d0b6147766b942cd63688f68fbe
Even Brenden <evenbrenden@gmail.com>
2020-08-05 12:06:21 +0200

    NixOS + home-manager: Use shared (pinned) nixpkgs

--- home/home.nix
+++ home/home.nix
@@ -1,11 +1,7 @@
 { config, ... }:
 
 let
-  pinnedNixpkgs = import(builtins.fetchTarball {
-    name = "nixos-unstable-a45f68ccac";
-    url = "https://github.com/nixos/nixpkgs/archive/a45f68ccac476dc37ddf294530538f2f2cce5a92.tar.gz";
-    sha256 = "0i19mrky9m73i601hczyfk25qqyr3j75idb72imdz55szc4vavzc";
-  }) {};
+  pinnedNixpkgs = import(import ./pinned_nixpkgs.nix) {};
 in
   {
     programs = {
