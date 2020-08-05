257b1c85fc6c8ce0b48e520107da7f2574f3fb5a
Even Brenden <evenbrenden@gmail.com>
2020-08-05 12:07:39 +0200

    Fix home-install.sh

--- home-install.sh
+++ home-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm -rf ~/.config/nixpkgs \
-    && cp pinned_nixpkgs.nix ~/.config/nixpkgs \
     && cp -r home ~/.config/nixpkgs \
+    && cp pinned_nixpkgs.nix ~/.config/nixpkgs \
     && home-manager switch \
     && i3-msg reload \
     && i3-msg restart
