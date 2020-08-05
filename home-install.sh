6b68a05b584ee428c1173978dd9ed979218e36a8
Even Brenden <evenbrenden@gmail.com>
2020-08-05 12:06:21 +0200

    NixOS + home-manager: Use shared (pinned) nixpkgs

--- home-install.sh
+++ home-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 rm -rf ~/.config/nixpkgs \
+    && cp pinned_nixpkgs.nix ~/.config/nixpkgs \
     && cp -r home ~/.config/nixpkgs \
     && home-manager switch \
     && i3-msg reload \
