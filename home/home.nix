da78892f86db2a0ef2c8de1df1bbc92e82541d49
Even Brenden <evenbrenden@gmail.com>
2020-08-10 14:46:52 +0200

    bashrc: Set NIX_PATH for non-NixOS installs

--- home/home.nix
+++ home/home.nix
@@ -58,6 +58,7 @@
     };
   };
   home.file = {
+    "nixpkgs.url".source = ../nixpkgs.url;
     ".bashrc".source = ./dotfiles/bashrc;
     ".gitignore".source = ./dotfiles/gitignore;
     ".gitconfig".source = ./dotfiles/gitconfig;
