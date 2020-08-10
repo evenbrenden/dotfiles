9e87c73dafc862cbcf7a0c6ade3f389c62aebdc1
Even Brenden <evenbrenden@gmail.com>
2020-08-10 16:32:22 +0200

    home: Set NIXPKGS for bash sessions
    And do e.g. 'nix-env -f $NIXPATH ...' (no need for <nixpkgs>).
    
    Bye-bye NIX_PATH and Nix channels.

--- home/home.nix
+++ home/home.nix
@@ -58,7 +58,10 @@
     };
   };
   home.file = {
-    ".bashrc".source = ./dotfiles/bashrc;
+    ".bashrc".text = with builtins;
+      (readFile ./dotfiles/bashrc)
+      +
+      "export NIXPKGS=${readFile ../nixpkgs.url}";
     ".gitignore".source = ./dotfiles/gitignore;
     ".gitconfig".source = ./dotfiles/gitconfig;
     "bin/toggle_keyboard_layout.py".source = ./dotfiles/toggle_keyboard_layout.py;
