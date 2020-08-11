872ebcd270df418c3c6bd7cce74f77e3a539eff5
Even Brenden <evenbrenden@gmail.com>
2020-08-11 16:45:21 +0200

    home-vm: Add some packages and set ~/.profile

--- home/home-vm.nix
+++ home/home-vm.nix
@@ -3,6 +3,21 @@
 {
   imports = [ ./home-common.nix ];
 
+  home.packages = with pkgs;
+    [
+      curl
+      dbeaver
+      git
+      postman
+      rclone
+      remmina
+      unzip
+      zip
+    ];
+
+  # For non-NixOS systems
+  home.file.".profile".source = ./dotfiles/profile;
+
   xdg.configFile."i3/config".text = with builtins;
     (readFile ./dotfiles/i3config-common)
     +
