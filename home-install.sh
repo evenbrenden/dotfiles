6232834b417473107b1770d64a99da48ea9ad17d
Even Brenden <evenbrenden@gmail.com>
2020-08-11 09:56:49 +0200

    home-install: Check # args

--- home-install.sh
+++ home-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $# -ne 1 ]]; then
+    echo 'home-install [rm|vm]'
+    exit 1
+fi
+
 nixpkgs=$(cat nixpkgs.url)
 home-manager -f home/home-$1.nix -I nixpkgs=$nixpkgs switch \
     && i3-msg reload \
