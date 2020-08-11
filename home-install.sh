0389d36d18d00f08521b24952614ce4e9d7f3638
Even Brenden <evenbrenden@gmail.com>
2020-08-11 09:27:06 +0200

    home: RM/VM split

--- home-install.sh
+++ home-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 nixpkgs=$(cat nixpkgs.url)
-home-manager -f home/home.nix -I nixpkgs=$nixpkgs switch \
+home-manager -f home/home-$1.nix -I nixpkgs=$nixpkgs switch \
     && i3-msg reload \
     && i3-msg restart
