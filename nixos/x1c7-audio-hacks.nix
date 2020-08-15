27e0503d07d1567537110e9149cd7bf42bd43994
Even Brenden <evenbrenden@gmail.com>
2020-08-15 11:43:27 +0200

    Rename headphone-pops-fix

--- nixos/x1c7-audio-hacks.nix
+++ nixos/x1c7-audio-hacks.nix
@@ -25,21 +25,25 @@
   ];
 
   systemd = {
-    services = {
+    services =
+      let
+        sleep_targets = [ "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
+      in
+      {
       # This addresses the "(...) slight "pop" in headphones when content volume transitions to/from 0."
-      # It attenuates the pops to the point that they are virtually inaudible. Note that the pops are
-      # "Also present on mainline and in Windows."
-      attenuate-headphone-pops = {
-        description = "Attenuate headphone pops";
+      # It attenuates the pops to the point that they are virtually inaudible
+      # Note that the pops are "Also present on mainline and in Windows."
+      headphone-pops-fix = {
+        description = "Headphone pops fix";
         script = ''
           # https://gist.github.com/hamidzr/dd81e429dc86f4327ded7a2030e7d7d9#gistcomment-3154512
           hda-verb /dev/snd/hwC0D0 0x1d SET_PIN_WIDGET_CONTROL 0x0
-          # This was found using hda-analyzer and has the same effect (both are needed)
+          # This node behaves the same way (and this is the default value)
           hda-verb /dev/snd/hwC0D0 0x1a SET_PIN_WIDGET_CONTROL 0x0
         '';
         path = [ pkgs.alsaTools ];
-        after = [ "multi-user.target" "sound.target" "graphical.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
-        wantedBy = [ "sound.target" "suspend.target" "hibernate.target" "hybrid-sleep.target" ];
+        after = [ "multi-user.target" "sound.target" "graphical.target" ] ++ sleep_targets;
+        wantedBy = [ "sound.target" ] ++ sleep_targets;
       };
     };
     user.services = {
