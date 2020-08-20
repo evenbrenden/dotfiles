d43bdc84bdecd543276b4a302249c026a55f4d61
Even Brenden <evenbrenden@gmail.com>
2020-08-20 07:23:36 +0200

    xss-lock: Transfer sleep lock
    xsecurelock recommandation.

--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -3,6 +3,7 @@
 {
   programs.xss-lock = {
     enable = true;
+    extraOptions = [ "--transfer-sleep-lock" ];
     lockerCommand = ''env XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 XSECURELOCK_KEY_XF86Display_COMMAND='${pkgs.autorandr}/bin/autorandr --change' ${pkgs.xsecurelock}/bin/xsecurelock'';
   };
 
