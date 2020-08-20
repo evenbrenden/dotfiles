d32ac4e03b51b8201f95a05249dad65527040dcb
Even Brenden <evenbrenden@gmail.com>
2020-08-20 08:54:50 +0200

    Tidy up

--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -4,7 +4,12 @@
   programs.xss-lock = {
     enable = true;
     extraOptions = [ "--transfer-sleep-lock" ];
-    lockerCommand = ''env XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 XSECURELOCK_KEY_XF86Display_COMMAND='${pkgs.autorandr}/bin/autorandr --change' ${pkgs.xsecurelock}/bin/xsecurelock'';
+    lockerCommand = ''
+      env \
+      XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 \
+      XSECURELOCK_KEY_XF86Display_COMMAND='${pkgs.autorandr}/bin/autorandr --change' \
+      ${pkgs.xsecurelock}/bin/xsecurelock
+    '';
   };
 
   services = {
