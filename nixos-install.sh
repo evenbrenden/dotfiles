de763b2b052b496b9dd8b38beece7e55d6cc874a
Even Brenden <evenbrenden@gmail.com>
2020-08-22 09:39:58 +0200

    NixOS: Update install script

--- nixos-install.sh
+++ nixos-install.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 if [[ $# -ne 1 ]]; then
-    echo 'nixos-install [x1c7|t490s]'
+    echo 'nixos-install [machine]'
     exit 1
 fi
 
+if [[ $1 == 't490s' ]]; then
+    config='../nrk-work-pc-config/t490s-configuration.nix'
+else
+    config="nixos/$1-configuration.nix"
+fi
+
+if [[ ! -f $config ]]; then
+    echo "$config not found"
+    exit 1;
+fi
+
 nixpkgs=$(cat nixpkgs.url)
-sudo nixos-rebuild -I nixos-config=nixos/$1-configuration.nix -I nixpkgs=$nixpkgs switch
+sudo nixos-rebuild -I nixos-config=$config -I nixpkgs=$nixpkgs switch
