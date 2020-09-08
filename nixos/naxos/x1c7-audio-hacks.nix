0ea1008258d1fc0800c8c912369b0686dc05434f
Even Brenden <evenbrenden@gmail.com>
2020-09-08 09:49:19 +0200

    Update nixpkgs URL
    Means using 5.8 kernel.

--- nixos/naxos/x1c7-audio-hacks.nix
+++ nixos/naxos/x1c7-audio-hacks.nix
@@ -8,11 +8,6 @@
         name = "alsa-hda-realtek-fix";
         patch = ./0001-ALSA-hda-realtek-Add-control-fixup-for-Lenovo-Thinkp.patch;
       }
-      # Need this for applying the fixup until kernel 5.8 is available
-      {
-        name = "snd-sof-intel-hda-common-thing";
-        patch = ./3e645a4add53eec22f3818c9da01c19191525096..b8d3ad51dfec3631763cfef3d30c16f40140058b.patch;
-      }
     ];
     extraModprobeConfig = ''options snd_sof_intel_hda_common hda_model=alc285-tpx1-dual-speakers'';
     # Debug option for verifying that the fixup has been applied
