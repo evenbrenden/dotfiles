26ede6c0d9c91504f0d83e97a6d5fb1d8ac9ea4b
Even Brenden <evenbrenden@gmail.com>
2020-09-10 18:17:01 +0200

    rclone-sync: Add missing dependency

--- pkgs/rclone-sync.nix
+++ pkgs/rclone-sync.nix
@@ -4,6 +4,11 @@ stdenv.mkDerivation {
   name = "rclone-sync";
   buildInputs = [ pkgs.python37 ];
   unpackPhase = ":";
-  installPhase = "install -m755 -D ${./rclone-sync.py} $out/bin/rclone-sync";
+  installPhase = ''
+    install -m755 -D ${./rclone-sync.py} $out/bin/rclone-sync
+    substituteInPlace $out/bin/rclone-sync --replace 'rclone-sync' 'temp'
+    substituteInPlace $out/bin/rclone-sync --replace 'rclone' '${pkgs.rclone}/bin/rclone'
+    substituteInPlace $out/bin/rclone-sync --replace 'temp' 'rclone-sync'
+  '';
 }
 
