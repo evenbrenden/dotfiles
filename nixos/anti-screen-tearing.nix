d08ca0274552b9d1c1ac10d0096884214ad38322
Even Brenden <evenbrenden@gmail.com>
2020-09-11 08:32:29 +0200

    picom: Backend back on xrender
    * paint-on-overlay is not supported anymore ("enabled whenever
      possible") and crashes picom (check journalctl) silently all the time
      (though this is not visible at all on the user side).
    * However, paint-on-overlay actually makes a difference (when backend
      is on glx) and some programs get black context menus and such without
      it.
    * However, with backend on xrender, xsecurelock is not super happy.
      XSECURELOCK_NO_COMPOSITE=1 seems to fix this (check xsecurelock
      README).

--- nixos/anti-screen-tearing.nix
+++ nixos/anti-screen-tearing.nix
@@ -2,10 +2,8 @@
 
 {
   services.picom = {
-    backend = "glx"; # For xsecurelock
     enable = true;
     vSync = false;
-    settings.paint-on-overlay = true;
   };
 
   services.xserver = {
--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -6,6 +6,7 @@
     extraOptions = [ "--transfer-sleep-lock" ];
     lockerCommand = ''
       env \
+      XSECURELOCK_NO_COMPOSITE=1 \
       XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 \
       XSECURELOCK_KEY_XF86Display_COMMAND='${pkgs.autorandr}/bin/autorandr --change' \
       ${pkgs.xsecurelock}/bin/xsecurelock
