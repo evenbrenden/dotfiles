5e4f31e70bceb78cce4e4e924786c4e73c6db83e
Even Brenden <evenbrenden@gmail.com>
2020-09-18 11:44:20 +0200

    Revert "jetbrains.rider: 2020.2.1 -> 2020.2.2"
    This reverts commit 08aac1c7bcdde2efdc62ab6bd8078559a9401ea2.

--- overlays/jetbrains.rider.nix
+++ overlays/jetbrains.rider.nix
@@ -3,11 +3,11 @@
     jetbrains = super.jetbrains // {
       rider = super.jetbrains.rider.overrideDerivation (_: rec {
         name = "rider-${version}";
-        version = "2020.2.2";
+        version = "2020.2.1";
 
         src = super.fetchurl {
           url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-          sha256 = "1v3n4mg8b55ni72bdgsgiwyqcvp9zhqlkqshscwfjggv0iai9r6p";
+          sha256 = "0xrk7n0mprzy7dfkx3vj5wasw5031jl61qkh89y6w031hp77vq7n";
         };
       });
     };
