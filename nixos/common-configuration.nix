5b887a0b3630e4bc7260a06ae7f97909c7285a21
Even Brenden <evenbrenden@gmail.com>
2020-10-20 21:44:12 +0200

    i3: 4.18.1 -> 4.18.3

--- nixos/common-configuration.nix
+++ nixos/common-configuration.nix
@@ -8,8 +8,8 @@
 
   # Programs
   nixpkgs.overlays = [
-    # Skipping i3wm 4.18.2 (https://github.com/i3/i3/issues/4159)
-    (import ../overlays/i3wm.nix)
+    # Skipping i3 4.18.2 (https://github.com/i3/i3/issues/4159)
+    (import ../overlays/i3.nix)
   ];
   nixpkgs.config = {
     allowUnfree = true;
