85005c3c52a7031cd11755e43efa2b20f7cb7b2f
Even Brenden <evenbrenden@gmail.com>
2020-11-03 15:01:21 +0100

    home: Add some vscode extensions and refactor

--- home/rm/home.nix
+++ home/rm/home.nix
@@ -1,73 +1,56 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ ../home-common.nix ];
+  imports = [
+    ../home-common.nix
+    ./vscode.nix
+  ];
 
   nixpkgs.overlays = [
     (import ../../overlays/jetbrains.rider.nix)
     (import ../../overlays/jotta-cli.nix)
   ];
 
-  home.packages = with pkgs;
-    [
-      # For dotfiles
-      autorandr
-      brightnessctl
-      dunst
-      networkmanagerapplet
-      libnotify
-      playerctl
+  home.packages = with pkgs; [
+    # For dotfiles
+    autorandr
+    brightnessctl
+    dunst
+    networkmanagerapplet
+    libnotify
+    playerctl
 
-      # User programs
-      abcde
-      arandr
-      cabal-install
-      chromium
-      dos2unix
-      firefox
-      flameshot
-      fzf
-      gimp
-      gparted
-      graphviz
-      irssi
-      jetbrains.rider
-      jotta-cli
-      jq
-      vlc
-      nomacs
-      pavucontrol
-      python3
-      python37Packages.virtualenv
-      (callPackage (import ../../pkgs/rclone-sync) {})
-      shellcheck
-      slack
-      snes9x-gtk
-      spotify
-      teams
-      transmission-gtk
-      udiskie
-      veracrypt
-    ];
-    programs.vscode = {
-      enable = true;
-      extensions = [ (pkgs.callPackage (import ../../pkgs/ms-vsliveshare-vsliveshare) {}) ];
-      userSettings = {
-        "editor.occurrencesHighlight" = true;
-        "editor.renderLineHighlight" = "none";
-        "editor.copyWithSyntaxHighlighting" = false;
-        "editor.highlightActiveIndentGuide" = false;
-        "workbench.editor.highlightModifiedTabs" = true;
-        "editor.renderIndentGuides" = false;
-        "editor.cursorBlinking" = "solid";
-        "editor.cursorStyle" = "block";
-        "editor.minimap.enabled" = false;
-        "editor.matchBrackets" = "never";
-        "editor.links" = false;
-        "workbench.editor.enablePreview" = false;
-        "FSharp.useSdkScripts" = true;
-      };
-    };
+    # User programs
+    abcde
+    arandr
+    cabal-install
+    chromium
+    dos2unix
+    firefox
+    flameshot
+    fzf
+    gimp
+    gparted
+    graphviz
+    irssi
+    jetbrains.rider
+    jotta-cli
+    jq
+    vlc
+    nomacs
+    pavucontrol
+    python3
+    python37Packages.virtualenv
+    (callPackage (import ../../pkgs/rclone-sync) {})
+    shellcheck
+    slack
+    snes9x-gtk
+    spotify
+    teams
+    transmission-gtk
+    udiskie
+    veracrypt
+  ];
 
   xdg.configFile."i3/config".text = with builtins;
     (readFile ../dotfiles/i3config-common)
---
+++ home/rm/vscode.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }:
+
+{
+  programs.vscode = {
+    enable = true;
+    extensions =
+      [ (pkgs.callPackage (import ../../pkgs/ms-vsliveshare-vsliveshare) {}) ]
+      ++
+      pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "ionide-fsharp";
+          publisher = "ionide";
+          version = "4.17.0";
+          sha256 = "1z1bdrbybnx3i4x461dbzz0a4p5v2rndszm7dwk1sqv6j8fixc4b";
+        }
+        {
+          name = "csharp";
+          publisher = "ms-dotnettools";
+          version = "1.23.4";
+          sha256 = "1x90r4c90ylvwx4bmr6pmlzmx55vbkc91a4sqhi129rdvq2miv4w";
+        }
+        {
+          name = "vscode-graphviz";
+          publisher = "joaompinto";
+          version = "0.0.6";
+          sha256 = "17z5zgr8l94mj8dgqxwsrpixnkz0778fp1g4rxc7i56wb1zbik3w";
+        }
+      ];
+    userSettings = {
+      "editor.occurrencesHighlight" = true;
+      "editor.renderLineHighlight" = "none";
+      "editor.copyWithSyntaxHighlighting" = false;
+      "editor.highlightActiveIndentGuide" = false;
+      "workbench.editor.highlightModifiedTabs" = true;
+      "editor.renderIndentGuides" = false;
+      "editor.cursorBlinking" = "solid";
+      "editor.cursorStyle" = "block";
+      "editor.minimap.enabled" = false;
+      "editor.matchBrackets" = "never";
+      "editor.links" = false;
+      "workbench.editor.enablePreview" = false;
+      "FSharp.useSdkScripts" = true;
+    };
+  };
+}
