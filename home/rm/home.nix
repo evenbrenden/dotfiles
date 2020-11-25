3fcac26b83d1a830e3410f3be9085712b411b30f
Even Brenden <evenbrenden@gmail.com>
2020-11-25 15:27:37 +0100

    Update comment

--- home/rm/home.nix
+++ home/rm/home.nix
@@ -9,7 +9,7 @@
   nixpkgs.overlays = [
     # JetBrains releases Rider quite often (so nixpkgs is usually behind)
     (import ../../overlays/jetbrains.rider.nix)
-    # Waiting for 0.7.35160 (https://github.com/jotta/jotta-cli-issues/issues/111)
+    # Waiting for 0.8.36055 (https://github.com/jotta/jotta-cli-issues/issues/111)
     (import ../../overlays/jotta-cli.nix)
   ];
 
