a56bfb85336dbac56ced66b7aca0f8f0b4f6ccbd
Even Brenden <evenbrenden@gmail.com>
2020-12-02 08:20:43 +0100

    dotnet: Add SDK 3.1

--- home/dotnet.nix
+++ home/dotnet.nix
@@ -1,18 +1,24 @@
 { config, pkgs, ... }:
 
+let
+  dotnet =
+    (with pkgs.dotnetCorePackages; combinePackages [
+      sdk_5_0
+      sdk_3_1 # For a certain project
+    ]);
+in
 {
-  home.packages = with pkgs; [
-    dotnet-sdk_5
-  ];
-
-  home.file =
-    let
-      # Workaround for .NET Core SDK installed with Nix (https://wiki.archlinux.org/index.php/.NET_Core)
-      dotnet_root = "export DOTNET_ROOT=${pkgs.dotnet-sdk_5}\n";
-      dotnet_tools = "PATH=$PATH:${config.home.homeDirectory}/.dotnet/tools\n";
-    in
-    {
-      ".bashrc".text = dotnet_root + dotnet_tools;
-      ".profile".text = dotnet_root + dotnet_tools;
-    };
+  home = {
+    packages = [ dotnet ];
+    file =
+      let
+        # Workaround for .NET Core SDK installed with Nix (https://wiki.archlinux.org/index.php/.NET_Core)
+        dotnet_root = "export DOTNET_ROOT=${dotnet}\n";
+        dotnet_tools = "PATH=$PATH:${config.home.homeDirectory}/.dotnet/tools\n";
+      in
+      {
+        ".bashrc".text = dotnet_root + dotnet_tools;
+        ".profile".text = dotnet_root + dotnet_tools;
+      };
+  };
 }
