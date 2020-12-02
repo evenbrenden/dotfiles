89ad192985ac3f33371263b9fd73f8e410aab6c1
Even Brenden <evenbrenden@gmail.com>
2020-12-02 08:55:17 +0100

    Revert "jetbrains.rider: 2020.2.4 -> 2020.3-EAP9"
    This reverts commit 3370a3fa9ae4c7ec3eeb45a10ac7e7cf633ab2a8.

--- overlays/jetbrains.rider.nix
+++ overlays/jetbrains.rider.nix
@@ -3,11 +3,11 @@
     jetbrains = super.jetbrains // {
       rider = super.jetbrains.rider.overrideDerivation (_: rec {
         name = "rider-${version}";
-        version = "2020.3-EAP9-203.5981.21.Checked";
+        version = "2020.2.4";
 
         src = super.fetchurl {
           url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-          sha256 = "1nwx8gd8fbpz9lii98wbhwymx8jbb0yxbn7i0mh4df5180g65pj3";
+          sha256 = "1anl48ifv5ayqn876dqckxc1b5fw1271pvamzf1vvk501wv6dpaf";
         };
       });
     };
