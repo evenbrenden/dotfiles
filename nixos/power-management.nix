78451e629e1c8b36326bf8e1b6c91abd3a04366a
Even Brenden <evenbrenden@gmail.com>
2020-12-13 12:51:50 +0100

    xautolock: Don't do secure mode
    No need for that on a single user system like this.

--- nixos/power-management.nix
+++ nixos/power-management.nix
@@ -19,10 +19,9 @@
         xset s off
         xset -dpms
       '';
-      # Temporarily disable with: systemctl --user stop xautolock.service
+      # Temporarily disable with: xautolock -disable
       xautolock = {
         enable = true;
-        extraOptions = [ "-secure" ];
         locker = ''${pkgs.systemd}/bin/loginctl lock-session $XDG_SESSION_ID'';
         time = 15; # Minutes
       };
