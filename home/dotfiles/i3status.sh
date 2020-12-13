d3a8e469d02d43a4953543f275224433a7c1c752
Even Brenden <evenbrenden@gmail.com>
2020-12-13 12:34:26 +0100

    i3status: Refactor

--- home/dotfiles/i3status.sh
+++ home/dotfiles/i3status.sh
@@ -5,19 +5,20 @@ i3status | while :
 do
     read -r line
 
-    KEYBOARD_LAYOUT=$(setxkbmap -query | awk '/layout/{print $2}')
-    KEYBOARD_LAYOUT_COLOR="#ff00ff"
-
-    NOTIFICATIONS_PAUSED=$(dunstctl is-paused)
-    if [[ $NOTIFICATIONS_PAUSED = "false" ]]; then
-        NOTIFICATIONS_STATE_PRINT="on"
-        NOTIFICATIONS_STATE_COLOR="#ffffff"
-    elif [[ $NOTIFICATIONS_PAUSED = "true" ]]; then
-        NOTIFICATIONS_STATE_PRINT="off"
-        NOTIFICATIONS_STATE_COLOR="#ff0000"
+    NOTIFY_PAUSED=$(dunstctl is-paused)
+    if [[ $NOTIFY_PAUSED = 'false' ]]; then
+        NOTIFY_STATE_PRINT='notify'
+        NOTIFY_STATE_COLOR='#ffffff'
+    elif [[ $NOTIFY_PAUSED = 'true' ]]; then
+        NOTIFY_STATE_PRINT='paused'
+        NOTIFY_STATE_COLOR='#ff0000'
     fi
+    NOTIFY_ITEM="{ \"full_text\": \"$NOTIFY_STATE_PRINT\", \"color\":\"$NOTIFY_STATE_COLOR\" }"
 
-    data="[{ \"full_text\": \"$NOTIFICATIONS_STATE_PRINT\", \"color\":\"$NOTIFICATIONS_STATE_COLOR\" }, \
-        { \"full_text\": \"$KEYBOARD_LAYOUT\", \"color\":\"$KEYBOARD_LAYOUT_COLOR\" },"
+    KEYBOARD_LAYOUT=$(setxkbmap -query | awk '/layout/{print $2}')
+    KEYBOARD_LAYOUT_COLOR='#ff00ff'
+    KEYBOARD_ITEM="{ \"full_text\": \"$KEYBOARD_LAYOUT\", \"color\":\"$KEYBOARD_LAYOUT_COLOR\" }"
+
+    data="[$NOTIFY_ITEM, $KEYBOARD_ITEM,"
     echo "${line/[/$data}" || exit 1
 done
