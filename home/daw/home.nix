ca9d3b2d7ab0c9567f1314112e1c7c124392d52f
Even Brenden <evenbrenden@gmail.com>
2020-12-22 18:26:56 +0100

    renoise: Verify release tarball checksum

--- home/daw/home.nix
+++ home/daw/home.nix
@@ -18,7 +18,13 @@
 
     # Programs
     ardour
-    (renoise.override { releasePath = ./rns_324_linux_x86_64.tar.gz; })
+    (renoise.override {
+        releasePath = builtins.fetchTarball {
+          url = "file://${builtins.toString ./.}/rns_324_linux_x86_64.tar.gz";
+          sha256 = "0jwk9z62kk5dk95cbqasjrbag0qwvl2lix5k0pd98dmx05lxvbi5";
+        };
+      }
+    )
   ];
 
   home.file.".bashrc".text = builtins.readFile ../dotfiles/bashrc;
