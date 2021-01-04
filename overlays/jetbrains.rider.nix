c27165b63c7dc0a444c7142b5e6ab5c2f3f5b734
Even Brenden <evenbrenden@gmail.com>
2021-01-04 09:27:50 +0100

    jetbrains.rider: 2020.2.4 -> 2020.3.2

--- overlays/jetbrains.rider.nix
+++ overlays/jetbrains.rider.nix
@@ -3,11 +3,11 @@
     jetbrains = super.jetbrains // {
       rider = super.jetbrains.rider.overrideDerivation (_: rec {
         name = "rider-${version}";
-        version = "2020.2.4";
+        version = "2020.3.2";
 
         src = super.fetchurl {
           url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-          sha256 = "1anl48ifv5ayqn876dqckxc1b5fw1271pvamzf1vvk501wv6dpaf";
+          sha256 = "1dkgbd2nqkjcswf7j3pnrsaq9n5wk42abz2c4wgkrh1zrpgihd0j";
         };
       });
     };
