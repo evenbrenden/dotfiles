2a6fcbe1ae9f17159ae9355c57dbb68d149f6155
Even Brenden <evenbrenden@gmail.com>
2021-01-28 15:07:48 +0100

    jotta-cli: 0.9.38023 -> 0.9.39282

--- overlays/jotta-cli.nix
+++ overlays/jotta-cli.nix
@@ -2,15 +2,15 @@
   {
     jotta-cli = super.jotta-cli.overrideAttrs (_: rec {
       pname = "jotta-cli";
-      version = "0.9.38023";
+      version = "0.9.39282";
 
       src =
         let
           arch = "amd64";
         in
         super.fetchzip {
-          url = "https://repo.jotta.us/archives/linux/${arch}/jotta-cli-${version}_linux_${arch}.tar.gz";
-          sha256 = "0whbf7sb4i3g1f6yps3a6l3f0z3dg681ypax4snxw5vchi3h99kc";
+          url = "https://repo.jotta.us/archives-unstable/linux/${arch}/jotta-cli-${version}_linux_${arch}.tar.gz";
+          sha256 = "04v0ircyr5grhpkzljm2v08q455w4ql65a2927jkalmphlyn9jas";
           stripRoot = false;
         };
     });
