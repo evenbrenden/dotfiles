65bcf4e3ee8475cd9adc453641d36fdf5933b683
Even Brenden <evenbrenden@gmail.com>
2021-04-03 13:33:55 +0200

    sfizz: Use upstream derivation

--- pkgs/sfizz/default.nix
+++ pkgs/sfizz/default.nix
@@ -1,13 +1,8 @@
-{ stdenv,
-  fetchFromGitHub,
-  cmake,
-  pkgconfig,
-  cairo,
-  gnome3,
-  libjack2,
-  libsndfile,
-  libxkbcommon,
-  xorg }:
+{ lib, stdenv, fetchFromGitHub
+, libjack2, libsndfile, xorg, freetype, libxkbcommon
+, cairo, glib, gnome3, flac, libogg, libvorbis, libopus
+, cmake, pkg-config
+}:
 
 stdenv.mkDerivation rec {
   pname = "sfizz";
@@ -17,39 +12,49 @@ stdenv.mkDerivation rec {
     owner = "sfztools";
     repo = pname;
     rev = version;
-    sha256 = "1aqhaip3xdnfbsh39avxwma6r8p561pcnhybplxdkcagw3kmh5yx";
+    sha256 = "sha256-3RdY5+BPsdk6vctDy24w5aJsVOV9qzSgXs62Pm5UEKs=";
     fetchSubmodules = true;
   };
 
-  postPatch = ''
-    substituteInPlace \
-      ./editor/external/vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp \
-      --replace '/usr/bin/zenity' '${gnome3.zenity}/bin/zenity'
-  '';
-
-  nativeBuildInputs = [ cmake pkgconfig ];
-
   buildInputs = [
-    cairo
     libjack2
     libsndfile
-    libxkbcommon
+    flac
+    libogg
+    libvorbis
+    libopus
     xorg.libX11
+    xorg.libxcb
+    xorg.libXau
+    xorg.libXdmcp
     xorg.xcbutil
     xorg.xcbutilcursor
+    xorg.xcbutilrenderutil
     xorg.xcbutilkeysyms
+    xorg.xcbutilimage
+    libxkbcommon
+    cairo
+    glib
+    gnome3.zenity
+    freetype
   ];
+  nativeBuildInputs = [ cmake pkg-config ];
+
+  postPatch = ''
+  substituteInPlace editor/external/vstgui4/vstgui/lib/platform/linux/x11fileselector.cpp \
+    --replace '"/usr/bin/zenity' '"${gnome3.zenity}/bin/zenity'
+  '';
 
   cmakeFlags = [
     "-DCMAKE_BUILD_TYPE=Release"
     "-DSFIZZ_TESTS=ON"
   ];
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     homepage = "https://github.com/sfztools/sfizz";
     description = "SFZ jack client and LV2 plugin";
     license = licenses.bsd2;
-    maintainers = [ maintainers.magnetophon maintainers.evenbrenden ];
+    maintainers = [ maintainers.magnetophon ];
     platforms = platforms.all;
     badPlatforms = platforms.darwin;
   };
