d2a797bfc6278d5f67974c1e3301b84050e2f6c6
Even Brenden <evenbrenden@gmail.com>
2021-04-25 18:37:00 +0200

    reaper: 6.26 -> 6.28

--- overlays/reaper.nix
+++ overlays/reaper.nix
@@ -3,11 +3,11 @@
     reaper = super.reaper.overrideAttrs (_: rec {
 
       pname = "reaper";
-      version = "6.26";
+      version = "6.28";
 
       src = super.fetchurl {
         url = "https://www.reaper.fm/files/${super.lib.versions.major version}.x/reaper${builtins.replaceStrings ["."] [""] version}_linux_x86_64.tar.xz";
-        sha256 = "0a7kznj0qn27r5313jrgcr2fqf7njgd4b0p2yh0brrwd7j2bw50k";
+        sha256 = "07lr20dr1xjbhg0qrrdk63f0zwwzrfkdbsp863rn6hdb3y6x5hfz";
       };
     });
   }
