196135ef9a54c269a52aea555a19084be2935f52
Even Brenden <evenbrenden@gmail.com>
2021-04-26 10:48:26 +0200

    jetbrains.rider: 2020.3.4 -> 2021.1.2

--- overlays/jetbrains.rider.nix
+++ overlays/jetbrains.rider.nix
@@ -3,12 +3,19 @@
     jetbrains = super.jetbrains // {
       rider = super.jetbrains.rider.overrideDerivation (_: rec {
         name = "rider-${version}";
-        version = "2020.3.4";
+        version = "2021.1.2";
 
         src = super.fetchurl {
           url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-          sha256 = "1v99yqj83aw9j400z3v24n7xnyxzw5vm0b3rwd4yb8w3ajl59gq1";
+          sha256 = "1a28pi18j0cb2wxhw1vnfg9gqsgf2kyfg0hl4xgqp50gzv7i3aam";
         };
+
+        # https://github.com/NixOS/nixpkgs/pull/120589
+        patchPhase = ''
+          rm -rf lib/ReSharperHost/linux-x64/dotnet
+          mkdir -p lib/ReSharperHost/linux-x64/dotnet/
+          ln -s ${super.dotnet-sdk_5}/bin/dotnet lib/ReSharperHost/linux-x64/dotnet/dotnet
+        '';
       });
     };
   }
