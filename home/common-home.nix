b6477bf0cf03deca914b62c70c02a781ceab57b4
Even Brenden <evenbrenden@gmail.com>
2021-06-09 10:19:52 +0200

    Restructure

--- home/common-home.nix
+++ home/common-home.nix
@@ -2,49 +2,15 @@
 
 {
   # Programs
+  imports = [
+    (import ./neovim.nix)
+  ];
   nixpkgs.overlays = [
     (import ./jotta-cli.nix)
   ];
   programs = {
     home-manager.enable = true;
     man.enable = false;
-    neovim = {
-      enable = true;
-      extraConfig = pkgs.lib.strings.fileContents ./dotfiles/init.vim;
-      plugins = with pkgs.vimPlugins;
-        let
-          sfz-vim = pkgs.vimUtils.buildVimPlugin {
-            name = "sfz-vim";
-            src = pkgs.fetchFromGitHub {
-              owner = "sfztools";
-              repo = "sfz.vim";
-              rev = "4ec4ad05beacd1ec69dc37dc8137f92d4c673fef";
-              sha256 = "0brk6847n8wd8zb57wp7wjxyc7i3r0q29riv8ppy39j5lpdsbbss";
-            };
-          };
-          vim-fsharp = pkgs.vimUtils.buildVimPlugin {
-            name = "vim-fsharp";
-            src = pkgs.fetchFromGitHub {
-              owner = "wsdjeg";
-              repo = "vim-fsharp";
-              rev = "a4255ba4866fa5aba91fec342f98964cffbbc542";
-              sha256 = "0vlr90x4rp30a98k0g1g0fmwmp0slblp74fr8zaclyvbc8kwzimc";
-            };
-          };
-        in
-        [
-          fzf-vim
-          tcomment_vim
-          sfz-vim
-          vim-airline
-          vim-fsharp
-          vim-gitgutter
-          vim-markdown
-          vim-nix
-          vim-pico8-syntax
-          wmgraphviz-vim
-        ];
-    };
   };
   home.packages = with pkgs;
     [
---
+++ home/neovim.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    extraConfig = pkgs.lib.strings.fileContents ./dotfiles/init.vim;
+    plugins = with pkgs.vimPlugins;
+      let
+        sfz-vim = pkgs.vimUtils.buildVimPlugin {
+          name = "sfz-vim";
+          src = pkgs.fetchFromGitHub {
+            owner = "sfztools";
+            repo = "sfz.vim";
+            rev = "4ec4ad05beacd1ec69dc37dc8137f92d4c673fef";
+            sha256 = "0brk6847n8wd8zb57wp7wjxyc7i3r0q29riv8ppy39j5lpdsbbss";
+          };
+        };
+        vim-fsharp = pkgs.vimUtils.buildVimPlugin {
+          name = "vim-fsharp";
+          src = pkgs.fetchFromGitHub {
+            owner = "wsdjeg";
+            repo = "vim-fsharp";
+            rev = "a4255ba4866fa5aba91fec342f98964cffbbc542";
+            sha256 = "0vlr90x4rp30a98k0g1g0fmwmp0slblp74fr8zaclyvbc8kwzimc";
+          };
+        };
+      in
+      [
+        fzf-vim
+        tcomment_vim
+        sfz-vim
+        vim-airline
+        vim-fsharp
+        vim-gitgutter
+        vim-markdown
+        vim-nix
+        vim-pico8-syntax
+        wmgraphviz-vim
+      ];
+  };
+}
