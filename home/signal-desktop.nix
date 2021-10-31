b7c20f3091a8eb71faa80032d938ca714a6c005f
Even Brenden <evenbrenden@gmail.com>
2021-10-31 11:48:45 +0100

    signal-desktop: 5.13.1 -> 5.22.0

--- home/signal-desktop.nix
+++ home/signal-desktop.nix
@@ -2,11 +2,11 @@
   {
     signal-desktop = super.signal-desktop.overrideAttrs (_: rec {
       pname = "signal-desktop";
-      version = "5.13.1";
+      version = "5.22.0";
 
       src = super.fetchurl {
         url = "https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-        sha256 = "0k3gbs6y19vri5n087wc6fdhydkis3h6rhxd3w1j9rhrb5fxjv3q";
+        sha256 = "1y88qw57wk187fjb05zqvagv4pamc8171xwvznqb2k0vclsg82j8";
       };
     });
   }
