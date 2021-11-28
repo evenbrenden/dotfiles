14b6febc9c714902212b4dd70b70394401c349dc
Even Brenden <evenbrenden@gmail.com>
2021-11-28 22:03:46 +0100

    sof-firmware: 1.8 -> 1.9.2

--- nixos/naxos/sof-firmware.nix
+++ nixos/naxos/sof-firmware.nix
@@ -3,20 +3,21 @@
   {
     sof-firmware = super.sof-firmware.overrideAttrs (_: rec {
 
-      version = "1.8";
+      version = "1.9.2";
 
       src = super.fetchFromGitHub {
         owner = "thesofproject";
         repo = "sof-bin";
         rev = "v${version}";
-        sha256 = "sha256-NPbzDXZoZVWriV3klemX59ACnAlb357A5V/GbmzshyA=";
+        sha256 = "sha256-aEoLlP34rorllCdTneATlCHnmhJD/VABV9Uv4dDmShA=";
       };
 
       installPhase = ''
-        cd v${version}.x
+        cd "v${super.lib.versions.majorMinor version}.x"
         mkdir -p $out/lib/firmware/intel/
         cp -a sof-v${version} $out/lib/firmware/intel/sof
         cp -a sof-tplg-v${version} $out/lib/firmware/intel/sof-tplg
+        runHook postInstall
       '';
     });
   }
