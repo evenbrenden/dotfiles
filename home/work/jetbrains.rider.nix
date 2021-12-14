8890b4e4cfbc924f8881a35b6060a114c4df759f
Even Brenden <evenbrenden@gmail.com>
2021-12-14 12:01:08 +0100

    rider: 2021.1.5 -> 2021.2.2

--- home/work/jetbrains.rider.nix
+++ home/work/jetbrains.rider.nix
@@ -3,11 +3,11 @@
     jetbrains = super.jetbrains // {
       rider = super.jetbrains.rider.overrideDerivation (_: rec {
         name = "rider-${version}";
-        version = "2021.1.5";
+        version = "2021.2.2";
 
         src = super.fetchurl {
           url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-          sha256 = "sha256:17mxqh6p9jby5qrjqaq5km0j8k1bp8061ch2j059ka3n4ycxy7ph";
+          sha256 = "17xx8mz3dr5iqlr0lsiy8a6cxz3wp5vg8z955cdv0hf8b5rncqfa";
         };
       });
     };
