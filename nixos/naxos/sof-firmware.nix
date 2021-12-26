f105181254746c6b7c09c937dbfce0246e0c5950
Even Brenden <evenbrenden@gmail.com>
2021-12-26 21:33:00 +0100

    sof-firmware: 1.9.3 -> 2.0

--- nixos/naxos/sof-firmware.nix
+++ nixos/naxos/sof-firmware.nix
@@ -1,15 +1,15 @@
-# https://github.com/NixOS/nixpkgs/pull/150512
+# https://github.com/NixOS/nixpkgs/pull/152240
 (self: super:
   {
     sof-firmware = super.sof-firmware.overrideAttrs (_: rec {
 
-      version = "1.9.3";
+      version = "2.0";
 
       src = super.fetchFromGitHub {
         owner = "thesofproject";
         repo = "sof-bin";
         rev = "v${version}";
-        sha256 = "0dpwpk91xhawpjc2lyq0748ln822z30wn18pcyylp3ph95rv00cr";
+        sha256 = "sha256-pDxNcDe/l1foFYuHB0w3YZidKIeH6h0IuwRmMzeMteE=";
       };
     });
   }
