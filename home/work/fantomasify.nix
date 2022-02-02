bb72cae37888cb3b8f09da181c3ae830b621011f
Even Brenden <evenbrenden@gmail.com>
2022-02-02 16:37:35 +0100

    Add fantomas helper script

---
+++ home/work/fantomasify.nix
@@ -0,0 +1,14 @@
+{ pkgs }:
+let
+  git = "${pkgs.git}/bin/git";
+in
+  pkgs.writeScriptBin
+    "fantomasify"
+    ''
+      #! /usr/bin/env bash
+
+      for file in $(git diff --name-only)
+      do
+          dotnet fantomas $file
+      done
+    ''
