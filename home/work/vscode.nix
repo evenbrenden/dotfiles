96fa0cd699e114fddd4b1fce4deacd71dd48cbc5
Even Brenden <evenbrenden@gmail.com>
2022-02-10 09:29:55 +0100

    vscode: Declare using home.packages
    Plugins would stop working at some point when using the HM module.

--- home/work/vscode.nix
+++ home/work/vscode.nix
@@ -1,14 +1,12 @@
 # https://nixos.wiki/wiki/Visual_Studio_Code
-# Known errors:
-# -csharp extension fails to load. Not sure what it does, but ionide says that it needs it.
-# -vsliveshare fails to use the keychain. Just close the error dialog and be on your way.
-{ config, pkgs, ... }: {
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      ms-vsliveshare.vsliveshare
-      ionide.ionide-fsharp
-      ms-dotnettools.csharp
-    ];
-  };
-}
+{ pkgs, ... }:
+
+let
+  extensions = with pkgs.vscode-extensions; [
+    ms-vsliveshare.vsliveshare
+    ionide.ionide-fsharp
+    ms-dotnettools.csharp
+  ];
+  vscode-with-extensions =
+    pkgs.vscode-with-extensions.override { vscodeExtensions = extensions; };
+in { home.packages = with pkgs; [ vscode-with-extensions ]; }
