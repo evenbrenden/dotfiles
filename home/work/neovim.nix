1a1588e3ef2020790e53913e92b20b945c601656
Even Brenden <evenbrenden@gmail.com>
2022-02-25 09:05:37 +0100

    Move ionide-vim to work

---
+++ home/work/neovim.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  programs.neovim = {
+    plugins = with pkgs.vimPlugins;
+      let
+        ionide-vim = pkgs.vimUtils.buildVimPlugin {
+          name = "ionide-vim";
+          src = pkgs.fetchFromGitHub {
+            owner = "ionide";
+            repo = "Ionide-vim";
+            rev = "6eb5de0b13cee781d0ccc0559d614ea032967293";
+            sha256 = "06psx9r82m29vs44r3n0diliiwg1dnv6gj0n6l9a9p5w2f68zjm7";
+          };
+        };
+
+      in [
+        ionide-vim # dotnet tool install -g fsautocomplete
+      ];
+  };
+}
