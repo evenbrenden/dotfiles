c74b2478a9649cac6c2d82fec08b24132f347f05
Even Brenden <evenbrenden@gmail.com>
2022-04-18 12:28:36 +0200

    picom: Unset fade (currently default)

--- nixos/anti-screen-tearing.nix
+++ nixos/anti-screen-tearing.nix
@@ -4,6 +4,8 @@
   services.picom = {
     backend = "xr_glx_hybrid"; # For xsecurelock
     enable = true;
+    fade =
+      false; # https://github.com/google/xsecurelock/issues/97#issuecomment-1100903794
     vSync = true;
   };
 
