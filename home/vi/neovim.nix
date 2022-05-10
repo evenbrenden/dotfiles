846bdaef262d7b80e76af9eef610565f7f725829
Even Brenden <evenbrenden@gmail.com>
2022-05-10 17:44:42 +0200

    Revert "Conceal code blocks in LSP popups"
    This reverts commit f9d16e0a9bb95cf5719a9040711b0453c9f00eb4.

--- home/vi/neovim.nix
+++ home/vi/neovim.nix
@@ -9,8 +9,5 @@
       rev = "v${version}";
       sha256 = "sha256-eYYaHpfSaYYrLkcD81Y4rsAMYDP1IJ7fLJJepkACkA8=";
     };
-
-    # https://github.com/neovim/neovim/issues/16114
-    patches = [ ./0001-Use-markdown.vim-files-from-vim-markdown.patch ];
   });
 })
