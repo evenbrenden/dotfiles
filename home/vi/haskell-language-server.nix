865e00075ea1a314cd31de2b58c56f5070ce8345
Even Brenden <evenbrenden@gmail.com>
2022-05-12 13:00:08 +0200

    haskell-language-server: 1.6.1.1 -> 1.7.0.0

--- home/vi/haskell-language-server.nix
+++ home/vi/haskell-language-server.nix
@@ -2,7 +2,7 @@
   haskell-language-server = super.haskell-language-server.overrideAttrs
     (_: rec {
       pname = "haskell-language-server";
-      version = "1.6.1.1";
-      sha256 = "03z650zk7ma0gsmb7mirzarwg0535kwmwyf0h8a89s8bqfcd5mxf";
+      version = "1.7.0.0";
+      sha256 = "03z650zk7ma0gsmb7mirzarwg0535kwmwyf0h8a89s8bqfcd5mxg";
     });
 })
