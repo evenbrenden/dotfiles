2fa1f8e93941c0d99851fcc88f4200c5efb0c5a1
Even Brenden <evenbrenden@gmail.com>
2022-05-13 16:35:50 +0200

    Move more stuff from Vimscript to Lua

--- home/dotfiles/neovim-init.lua
+++ home/dotfiles/neovim-init.lua
@@ -27,15 +27,31 @@ vim.opt.wildmenu = true
 -- fzf
 vim.api.nvim_set_keymap("n", "<leader>f", ":GFiles<CR>", key_opts)
 vim.api.nvim_set_keymap("n", "<leader>g", ":GGrep<CR>", key_opts)
+-- https://github.com/junegunn/fzf.vim#example-git-grep-wrapper
+local ggrep_cmd =
+    "command! -bang -nargs=* GGrep call fzf#vim#grep('git grep --line-number" ..
+        " -- '.shellescape(<q-args>), 0, fzf#vim#with_preview({'dir': system" ..
+        "list('git rev-parse --show-toplevel')[0]}), <bang>0)"
+vim.cmd(ggrep_cmd)
+
+-- GitGutter
+vim.opt.updatetime = 100
 
 -- GraphViz
 vim.api.nvim_set_keymap("n", "<localleader>g", ":GraphvizCompile<CR>", key_opts)
+vim.g.WMGraphviz_output = 'svg'
 
 -- Markdown
 vim.opt.concealcursor = 'nc'
 
 -- Neoformat
 vim.api.nvim_set_keymap("n", "<localleader>f", ":Neoformat<CR>", key_opts)
+vim.g.neoformat_enabled_python = {'autopep8'}
+vim.g.neoformat_enabled_haskell = {'brittany'}
+vim.g.neoformat_haskell_brittany = {exe = 'brittany', args = {'--indent=2'}}
+vim.g.neoformat_enabled_lua = {'luaformat'}
+vim.g.neoformat_enabled_nix = {'nixfmt'}
+vim.g.neoformat_enabled_shell = {'shfmt'}
 
 require 'neovim-completion'
 require 'neovim-lsp'
