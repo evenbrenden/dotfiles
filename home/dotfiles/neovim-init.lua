eec8900c2c5dd7a43b848e6a1c654747387eb743
Even Brenden <evenbrenden@gmail.com>
2022-05-14 09:01:10 +0200

    Refactor neovim-init.lua

--- home/dotfiles/neovim-init.lua
+++ home/dotfiles/neovim-init.lua
@@ -36,11 +36,9 @@ vim.api.nvim_set_keymap("n", "wt", ":ToggleWhitespace<CR>", key_opts)
 vim.api.nvim_set_keymap("n", "<leader>f", ":GFiles<CR>", key_opts)
 vim.api.nvim_set_keymap("n", "<leader>g", ":GGrep<CR>", key_opts)
 -- https://github.com/junegunn/fzf.vim#example-git-grep-wrapper
-local ggrep_cmd =
-    "command! -bang -nargs=* GGrep call fzf#vim#grep('git grep --line-number" ..
-        " -- '.shellescape(<q-args>), 0, fzf#vim#with_preview({'dir': system" ..
-        "list('git rev-parse --show-toplevel')[0]}), <bang>0)"
-vim.cmd(ggrep_cmd)
+vim.cmd("command! -bang -nargs=* GGrep call fzf#vim#grep('git grep --line-nu" ..
+            "mber -- '.shellescape(<q-args>), 0, fzf#vim#with_preview({'dir'" ..
+            ": systemlist('git rev-parse --show-toplevel')[0]}), <bang>0)")
 
 -- GitGutter
 vim.opt.updatetime = 100
