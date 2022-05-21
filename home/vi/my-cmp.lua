362a0c086a4ad17e0d889bb36a08e3f72915ee36
Even Brenden <evenbrenden@gmail.com>
2022-05-21 14:28:38 +0200

    Use luasnip

--- home/vi/my-cmp.lua
+++ home/vi/my-cmp.lua
@@ -1,41 +1,39 @@
--- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#vim-vsnip
 function setupCompletion()
 
+    -- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
     local has_words_before = function()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and
                    vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(
-                       col, col):match('%s') == nil
+                       col, col):match("%s") == nil
     end
 
-    local feedkey = function(key, mode)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true,
-                                                             true), mode, true)
-    end
+    local luasnip = require("luasnip")
+    local cmp = require("cmp")
 
-    local cmp = require('cmp')
-
-    cmp.setup {
+    cmp.setup({
         completion = {autocomplete = false},
         mapping = {
-            ['<C-n>'] = cmp.mapping(function(fallback)
+            ["<C-n>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_next_item()
-                elseif vim.fn['vsnip#available'](1) == 1 then
-                    feedkey('<Plug>(vsnip-expand-or-jump)', '')
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
                 elseif has_words_before() then
                     cmp.complete()
                 else
                     fallback()
                 end
-            end, {'i', 's'}),
-            ['<C-p>'] = cmp.mapping(function()
+            end, {"i", "s"}),
+            ["<C-p>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
-                elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-                    feedkey('<Plug>(vsnip-jump-prev)', '')
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
                 end
-            end, {'i', 's'}),
+            end, {"i", "s"}),
             ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
             ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
             ['<CR>'] = cmp.mapping.confirm {
@@ -43,7 +41,7 @@ function setupCompletion()
             }
         },
         sources = cmp.config.sources({
-            {name = 'buffer'}, {name = 'nvim_lsp'}, {name = 'vsnip'}
+            {name = 'buffer'}, {name = 'nvim_lsp'}, {name = 'luasnip'}
         })
-    }
+    })
 end
