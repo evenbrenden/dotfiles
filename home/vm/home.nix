d7bf41ae26ee6c7d3cfb73705a8a1d4a05118b4c
Even Brenden <evenbrenden@gmail.com>
2022-05-28 11:56:19 +0200

    Manage Bash with Home Manager

--- home/vm/home.nix
+++ home/vm/home.nix
@@ -4,8 +4,6 @@
   imports = [ ../common-home.nix ];
 
   # This is only so that dmenu is able to list programs that are installed with Nix
-  programs.bash.enable =
-    true; # So that session variables are sourced in ~/.profile
   targets.genericLinux.enable =
     true; # So that Nix profile is added to XDG_DATA_DIRS
 }
