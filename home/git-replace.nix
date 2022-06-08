f62453f037e1ba1a5e2d9d649e72e789ff0f1251
Even Brenden <evenbrenden@gmail.com>
2022-06-08 08:07:16 +0200

    git-replace: Handle no matches

--- home/git-replace.nix
+++ home/git-replace.nix
@@ -12,5 +12,5 @@ in pkgs.writeScriptBin "git-replace" ''
   with=$2
 
   # Remember: Use single quotes and escape all things regex
-  ${git} grep -l "$what" | xargs sed -i s/"$what"/"$with"/g
+  ${git} grep -l "$what" | xargs -r sed -i s/"$what"/"$with"/g
 ''
