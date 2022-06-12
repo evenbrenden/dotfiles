eeecfae55ef3ae0d7a595019434aefa8913c17a3
Even Brenden <evenbrenden@gmail.com>
2022-06-12 10:49:39 +0200

    Try solarized
    Using i3-style to style i3wm config.

--- home/vi/vi.nix
+++ home/vi/vi.nix
@@ -76,9 +76,11 @@
         neoformat
         neofsharp-vim
         nvim-lspconfig
+        nvim-solarized-lua
         sfz-vim
         tcomment_vim
         vim-airline
+        vim-airline-themes
         vim-better-whitespace
         vim-gitgutter
         vim-hocon
