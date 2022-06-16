5c1c19eac013a0183b7533d38cc5832052b0de5f
Even Brenden <evenbrenden@gmail.com>
2022-06-16 06:42:21 +0200

    Disable VirtualBox Extension Pack

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -5,7 +5,8 @@
     docker.enable = true;
     virtualbox.host = {
       enable = true;
-      enableExtensionPack = true;
+      # https://discourse.nixos.org/t/virtualbox-keeps-getting-rebuilt/6612
+      enableExtensionPack = false;
     };
   };
 }
