d4b13662fc1efda8e1285aabb2cf012dafe9bfc6
Even Brenden <evenbrenden@gmail.com>
2022-06-23 15:04:26 +0200

    Refactor PATH prefixed programs

--- home/bash/bash.nix
+++ home/bash/bash.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 
 let
-  prompt = ''
+  prompt = let git-ps1-prefix = "PATH=$PATH:${pkgs.git}/bin"; # Needs git
+  in ''
     source ${pkgs.git}/share/git/contrib/completion/git-prompt.sh
     DEFAULT_PS1="\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
-    GIT_INFO="\$(PATH=$PATH:${pkgs.git}/bin __git_ps1 '\[\e[3m\](%s)\[\033[0m\] ')"
+    GIT_INFO="\$(${git-ps1-prefix} __git_ps1 '\[\e[3m\](%s)\[\033[0m\] ')"
     PS1="$DEFAULT_PS1""$GIT_INFO"
   '';
 in {
