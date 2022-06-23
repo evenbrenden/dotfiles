8a0c49bf72cf111555d0ad2f1468c2ddca56eb8c
Even Brenden <evenbrenden@gmail.com>
2022-06-23 13:08:46 +0200

    Move all things i3 to i3.nix
    Add sakura to miscellaneous packages.

---
+++ home/i3/i3.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    autorandr
+    brightnessctl
+    (import ./i3quo.nix { inherit pkgs; })
+    playerctl
+    sakura
+    (import ./toggle_keyboard_layout.nix { inherit pkgs; })
+    (import ./toggle_wifi.nix { inherit pkgs; })
+    xrandr-invert-colors
+  ];
+  xdg.configFile = {
+    "i3/config".source = ./config;
+    "i3status/config".source = ./statusconfig;
+  };
+}
