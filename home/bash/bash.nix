8eed21983b9dcd6ab7e68bfcfb78ebc6ba794cad
Even Brenden <evenbrenden@gmail.com>
2022-06-24 09:59:53 +0200

    Loosen up bash.nix

--- home/bash/bash.nix
+++ home/bash/bash.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }:
 
 let
-  prompt = let git-ps1-prefix = "PATH=$PATH:${pkgs.git}/bin"; # Needs git
-  in ''
+  prompt = ''
     source ${pkgs.git}/share/git/contrib/completion/git-prompt.sh
     DEFAULT_PS1="\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
-    GIT_INFO="\$(${git-ps1-prefix} __git_ps1 '\[\e[3m\](%s)\[\033[0m\] ')"
+    GIT_INFO="\$(__git_ps1 '\[\e[3m\](%s)\[\033[0m\] ')"
     PS1="$DEFAULT_PS1""$GIT_INFO"
   '';
 in {
@@ -15,4 +14,7 @@ in {
       builtins.concatStringsSep "\n" [ prompt (builtins.readFile ./bashrc) ];
     shellOptions = [ ]; # Set in initExtra
   };
+  home.packages = [
+    pkgs.git # For git-prompt.sh
+  ];
 }
