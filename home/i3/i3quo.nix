bc8ea8b61cc620cf74bdd32684b06c00bbd82c3c
Even Brenden <evenbrenden@gmail.com>
2022-06-24 10:03:18 +0200

    Rewrite i3quo with writeShellApplication

--- home/i3/i3quo.nix
+++ home/i3/i3quo.nix
@@ -1,33 +1,37 @@
 { pkgs }:
-let
-  awk = "${pkgs.gawk}/bin/awk";
-  dunstctl =
-    let dunstctl-prefix = "PATH=$PATH:${pkgs.dbus}/bin"; # Needs dbus-send
-    in "${dunstctl-prefix} ${pkgs.dunst}/bin/dunstctl";
-  i3status = "${pkgs.i3status}/bin/i3status";
-  setxkbmap = "${pkgs.xorg.setxkbmap}/bin/setxkbmap";
-in pkgs.writeScriptBin "i3quo" ''
-  #! /usr/bin/env bash
 
-  # https://docs.slackware.com/howtos:window_managers:keyboard_layout_in_i3
-  ${i3status} | while :; do
-      read -r line
+pkgs.writeShellApplication {
+  name = "i3quo";
+  runtimeInputs = with pkgs; [
+    gawk
+    dbus # dunstctl needs dbus-send
+    dunst
+    i3status
+    xorg.setxkbmap
+  ];
+  text = ''
+    #! /usr/bin/env bash
 
-      NOTIFY_PAUSED=$(${dunstctl} is-paused)
-      if [[ $NOTIFY_PAUSED = 'false' ]]; then
-          NOTIFY_STATE_PRINT='notify'
-          NOTIFY_STATE_COLOR='#ffffff'
-      elif [[ $NOTIFY_PAUSED = 'true' ]]; then
-          NOTIFY_STATE_PRINT='paused'
-          NOTIFY_STATE_COLOR='#ff0000'
-      fi
-      NOTIFY_ITEM="{ \"full_text\": \"$NOTIFY_STATE_PRINT\", \"color\":\"$NOTIFY_STATE_COLOR\" }"
+    # https://docs.slackware.com/howtos:window_managers:keyboard_layout_in_i3
+    i3status | while :; do
+        read -r line
 
-      KEYBOARD_LAYOUT=$(${setxkbmap} -query | ${awk} '/layout/{print $2}')
-      KEYBOARD_LAYOUT_COLOR='#ff00ff'
-      KEYBOARD_ITEM="{ \"full_text\": \"$KEYBOARD_LAYOUT\", \"color\":\"$KEYBOARD_LAYOUT_COLOR\" }"
+        NOTIFY_PAUSED=$(dunstctl is-paused)
+        if [[ $NOTIFY_PAUSED = 'false' ]]; then
+            NOTIFY_STATE_PRINT='notify'
+            NOTIFY_STATE_COLOR='#ffffff'
+        elif [[ $NOTIFY_PAUSED = 'true' ]]; then
+            NOTIFY_STATE_PRINT='paused'
+            NOTIFY_STATE_COLOR='#ff0000'
+        fi
+        NOTIFY_ITEM="{ \"full_text\": \"$NOTIFY_STATE_PRINT\", \"color\":\"$NOTIFY_STATE_COLOR\" }"
 
-      data="[$NOTIFY_ITEM, $KEYBOARD_ITEM,"
-      echo "''${line/[/$data}" || exit 1
-  done
-''
+        KEYBOARD_LAYOUT=$(setxkbmap -query | awk '/layout/{print $2}')
+        KEYBOARD_LAYOUT_COLOR='#ff00ff'
+        KEYBOARD_ITEM="{ \"full_text\": \"$KEYBOARD_LAYOUT\", \"color\":\"$KEYBOARD_LAYOUT_COLOR\" }"
+
+        data="[$NOTIFY_ITEM, $KEYBOARD_ITEM,"
+        echo "''${line/[/$data}" || exit 1
+    done
+  '';
+}
