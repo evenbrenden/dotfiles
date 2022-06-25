05be6fe32cba42472aa9cc355ace77dc327fe18b
Even Brenden <evenbrenden@gmail.com>
2022-06-25 10:14:13 +0200

    Rewrite some scripts with writeShellApplication
    Remove unnecessary shebangs.

--- home/i3/toggle_keyboard_layout.nix
+++ home/i3/toggle_keyboard_layout.nix
@@ -1,18 +1,21 @@
 { pkgs }:
-let
-  awk = "${pkgs.gawk}/bin/awk";
-  pkill = "${pkgs.procps}/bin/pkill";
-  setxkbmap = "${pkgs.xorg.setxkbmap}/bin/setxkbmap";
-in pkgs.writeScriptBin "toggle_keyboard_layout" ''
-  #! /usr/bin/env bash
 
-  layout=$(${setxkbmap} -query | ${awk} 'NR==3 {print $2}')
+pkgs.writeShellApplication {
+  name = "toggle_keyboard_layout";
+  runtimeInputs = with pkgs; [
+    gawk
+    procps # For pkill
+    xorg.setxkbmap
+  ];
+  text = ''
+    layout=$(setxkbmap -query | awk 'NR==3 {print $2}')
 
-  if [[ $layout == 'us' ]]; then
-      ${setxkbmap} -layout norwerty
-  else
-      ${setxkbmap} -layout us
-  fi
+    if [[ $layout == 'us' ]]; then
+        setxkbmap -layout norwerty
+    else
+        setxkbmap -layout us
+    fi
 
-  ${pkill} -x -USR1 i3status
-''
+    pkill -x -USR1 i3status
+  '';
+}
