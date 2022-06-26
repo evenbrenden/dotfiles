e81667fe40fa1ae5fdc1c86d35c59e5d78d272c2
Even Brenden <evenbrenden@gmail.com>
2022-06-26 08:18:00 +0200

    PS1: Use PROMPT_COMMAND

--- home/bash/bash.nix
+++ home/bash/bash.nix
@@ -3,9 +3,9 @@
 let
   set-prompt = ''
     source ${pkgs.git}/share/git/contrib/completion/git-prompt.sh
-    DEFAULT_PS1="\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
-    GIT_INFO="\$(__git_ps1 '\[\e[3m\](%s)\[\033[0m\] ')"
-    PS1="$DEFAULT_PS1""$GIT_INFO"
+    DEFAULT_PS1='\n\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] '
+    GIT_INFO='\[\e[3m\](%s)\[\033[0m\] '
+    PROMPT_COMMAND='__git_ps1 "$DEFAULT_PS1" "" "$GIT_INFO"'
   '';
 in {
   programs.bash = {
