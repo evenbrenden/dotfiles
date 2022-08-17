ee4f0606ec6fda7831cd2f862dada3ad552b072a
Even Brenden <evenbrenden@gmail.com>
2022-08-17 16:05:18 +0200

    nvim: 0.7.2

--- home/vi/neovim.nix
+++ home/vi/neovim.nix
@@ -1,13 +1,13 @@
 (self: super: {
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (_: rec {
     pname = "neovim-unwrapped";
-    version = "0.8.0-dev";
+    version = "0.7.2";
 
     src = super.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
-      rev = "9370e1c5111ee90f64260398b0623da4759f8f16";
-      sha256 = "sha256-EsXsNEee5Ca9XPPAsFt9U6uoMw5McEvkgqjkTmh57qA=";
+      rev = "e8ee6733926db83ef216497a1d660a173184ff39";
+      sha256 = "sha256-eKKQNM02Vhy+3yL2QV+0FSEpcniEa5Aq6hkAUIgLo1k=";
     };
   });
 })
