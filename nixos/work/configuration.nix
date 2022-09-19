a4dd81946c648ee94e7394113dffb5c612359916
Even Brenden <evenbrenden@gmail.com>
2022-09-19 12:46:02 +0200

    work: Add k9s

--- nixos/work/configuration.nix
+++ nixos/work/configuration.nix
@@ -16,6 +16,7 @@ in {
     dbeaver
     (unstable.google-cloud-sdk.withExtraComponents
       [ unstable.google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    k9s
     kubectl
     nodejs
     vault
