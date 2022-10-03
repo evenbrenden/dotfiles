cc154b4d4d1b7a272889d267ec4fd9eda9d1887c
Even Brenden <evenbrenden@gmail.com>
2022-10-03 10:04:42 +0200

    mimeapps: More defaults

--- home/home.nix
+++ home/home.nix
@@ -23,13 +23,20 @@
       enable = true;
       defaultApplications = {
         "application/pdf" = [ "okularApplication_pdf.desktop" ];
+        "application/x-extension-htm" = [ "firefox.desktop" ];
+        "application/x-extension-html" = [ "firefox.desktop" ];
+        "application/x-extension-shtml" = [ "firefox.desktop" ];
+        "application/x-extension-xhtml" = [ "firefox.desktop" ];
+        "application/x-extension-xht" = [ "firefox.desktop" ];
+        "application/xhtml+xml" = [ "firefox.desktop" ];
         "text/html" = [ "firefox.desktop" ];
-        "x-scheme-handler/about" = [ "firefox.desktop" ];
+        "x-scheme-handler/about" = [ "firefox.desktop" "appgate.desktop" ];
+        "x-scheme-handler/chrome" = [ "firefox.desktop" ];
         "x-scheme-handler/http" = [ "firefox.desktop" ];
         "x-scheme-handler/https" = [ "firefox.desktop" ];
         "x-scheme-handler/magnet" = [ "transmission-gtk.desktop" ];
         "x-scheme-handler/msteams" = [ "teams.desktop" ];
-        "x-scheme-handler/unknown" = [ "firefox.desktop" ];
+        "x-scheme-handler/unknown" = [ "firefox.desktop" "appgate.desktop" ];
       };
     };
   };
