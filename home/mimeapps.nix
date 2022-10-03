a7496852dbde543c94989102c36a9dde8420e4f4
Even Brenden <evenbrenden@gmail.com>
2022-10-03 10:42:00 +0200

    home: Refactor

---
+++ home/mimeapps.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/pdf" = [ "okularApplication_pdf.desktop" ];
+      "application/x-extension-htm" = [ "firefox.desktop" ];
+      "application/x-extension-html" = [ "firefox.desktop" ];
+      "application/x-extension-shtml" = [ "firefox.desktop" ];
+      "application/x-extension-xhtml" = [ "firefox.desktop" ];
+      "application/x-extension-xht" = [ "firefox.desktop" ];
+      "application/xhtml+xml" = [ "firefox.desktop" ];
+      "text/html" = [ "firefox.desktop" ];
+      "x-scheme-handler/about" = [ "firefox.desktop" "appgate.desktop" ];
+      "x-scheme-handler/chrome" = [ "firefox.desktop" ];
+      "x-scheme-handler/http" = [ "firefox.desktop" ];
+      "x-scheme-handler/https" = [ "firefox.desktop" ];
+      "x-scheme-handler/magnet" = [ "transmission-gtk.desktop" ];
+      "x-scheme-handler/msteams" = [ "teams.desktop" ];
+      "x-scheme-handler/unknown" = [ "firefox.desktop" "appgate.desktop" ];
+    };
+  };
+}
