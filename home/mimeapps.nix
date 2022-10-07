e9ee0a276b52e601f4d992bee41cec4f8987b4c7
Even Brenden <evenbrenden@gmail.com>
2022-10-07 11:06:44 +0200

    mimeapps: nomacs for SVG

--- home/mimeapps.nix
+++ home/mimeapps.nix
@@ -10,6 +10,7 @@
       "application/x-extension-shtml" = [ "firefox.desktop" ];
       "application/x-extension-xhtml" = [ "firefox.desktop" ];
       "application/x-extension-xht" = [ "firefox.desktop" ];
+      "image/svg+xml" = [ "org.nomacs.ImageLounge.desktop" ];
       "application/xhtml+xml" = [ "firefox.desktop" ];
       "text/html" = [ "firefox.desktop" ];
       "x-scheme-handler/about" = [ "firefox.desktop" "appgate.desktop" ];
