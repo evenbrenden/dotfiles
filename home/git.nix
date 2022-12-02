f124638d943d8843b24b3e953ae92803e61c3347
Even Brenden <evenbrenden@gmail.com>
2022-12-02 22:11:19 +0100

    Git: Add conditional config

--- home/git.nix
+++ home/git.nix
@@ -1,6 +1,15 @@
 { pkgs, ... }:
 
-{
+let
+  privateAddress = {
+    user = "evenbrenden";
+    host = "gmail.com";
+  };
+  workAddress = {
+    user = "even.steen.brenden";
+    host = "schibsted.com";
+  };
+in {
   home.packages = [ (import ./git-replace.nix { inherit pkgs; }) pkgs.tig ];
   programs.git = {
     enable = true;
@@ -9,9 +18,23 @@
       init.defaultBranch = "main";
       pull.rebase = true;
       push.default = "simple";
-      user.name = "Even Brenden";
-      # git config user.email [ADDRESS]
     };
     ignores = [ ".direnv/" ".envrc" "*.swp" ];
+    includes = [
+      {
+        condition = "hasconfig:remote.*.url:**/*github.com*/**";
+        contents.user = {
+          name = "Even Brenden";
+          email = privateAddress.user + "@" + privateAddress.host;
+        };
+      }
+      {
+        condition = "hasconfig:remote.*.url:**/*github.schibsted.io*/**";
+        contents.user = {
+          name = "Even Brenden";
+          email = workAddress.user + "@" + workAddress.host;
+        };
+      }
+    ];
   };
 }
