62f602363c299922f9ad3b385e5f1afbc2f61a1f
Even Brenden <evenbrenden@gmail.com>
2022-12-08 07:45:04 +0100

    Add custom Parcellite service

---
+++ home/parcellite.nix
@@ -0,0 +1,47 @@
+# TODO Add tray icon option and make PR
+
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let cfg = config.services.parcellite;
+in {
+  meta.maintainers = [ maintainers.gleber ];
+
+  options.services.parcellite = {
+    enable = mkEnableOption "Parcellite";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.parcellite;
+      defaultText = literalExpression "pkgs.parcellite";
+      example = literalExpression "pkgs.clipit";
+      description = "Parcellite derivation to use.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    assertions = [
+      (lib.hm.assertions.assertPlatform "services.parcellite" pkgs
+        lib.platforms.linux)
+    ];
+
+    home.packages = [ cfg.package ];
+
+    systemd.user.services.parcellite = {
+      Unit = {
+        Description = "Lightweight GTK+ clipboard manager";
+        Requires = [ "tray.target" ];
+        After = [ "graphical-session-pre.target" "tray.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+
+      Install = { WantedBy = [ "graphical-session.target" ]; };
+
+      Service = {
+        ExecStart = "${cfg.package}/bin/${cfg.package.pname} --no-icon";
+        Restart = "on-abort";
+      };
+    };
+  };
+}
