5026e8c7939db837d6af53add51f10310d06667f
Even Brenden <evenbrenden@gmail.com>
2022-12-09 15:32:13 +0100

    Remove comment

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,6 +1,5 @@
 { pkgs, username }:
 
-# https://discourse.nixos.org/t/set-up-vagrant-with-libvirt-qemu-kvm-on-nixos/14653
 {
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
   environment.systemPackages = [ pkgs.vagrant ];
