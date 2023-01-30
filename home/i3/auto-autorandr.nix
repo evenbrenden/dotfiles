8beaeb501e9800fecf58c4f3c5d57adb7a84fe13
Even Brenden <evenbrenden@gmail.com>
2023-01-30 08:57:25 +0100

    Format auto-autorandr

--- home/i3/auto-autorandr.nix
+++ home/i3/auto-autorandr.nix
@@ -4,11 +4,10 @@ pkgs.writeShellApplication {
   name = "auto-autorandr";
   runtimeInputs = with pkgs; [ autorandr ];
   text = ''
-    if [ -z "$(autorandr --detected)" ]
-    then
-      autorandr --change clone-largest
+    if [ -z "$(autorandr --detected)" ]; then
+        autorandr --change clone-largest
     else
-      autorandr --change
+        autorandr --change
     fi
   '';
 }
