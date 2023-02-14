27dc295a38a2171e343247b9ecf71061c5817dad
Even Brenden <evenbrenden@gmail.com>
2023-02-14 10:05:36 +0100

    Override Spotify desktop entry

--- home/home.nix
+++ home/home.nix
@@ -18,6 +18,10 @@
       "VeraCrypt/Favorite Volumes.xml".source =
         ./dotfiles/veracrypt-favorite-volumes.xml;
     };
+    desktopEntries.spotify = {
+      name = "Spotify";
+      exec = "spotify --force-device-scale-factor=1.5";
+    };
   };
 
   # Programs
