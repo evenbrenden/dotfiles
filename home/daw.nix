c1d74caf7ff32f601da903ce0687548ce1e4638a
Even Brenden <evenbrenden@users.noreply.github.com>
2023-06-23 13:21:52 +0200

    Set UHE_RT_PRIO

--- home/daw.nix
+++ home/daw.nix
@@ -17,6 +17,7 @@
     LADSPA_PATH = "$HOME/.ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa";
     LV2_PATH = "$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2";
     LXVST_PATH = "$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
+    UHE_RT_PRIO = 64; # For u-he plugins
     VST_PATH = "$HOME/.vst:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst";
     VST3_PATH = "$HOME/.vst3:$HOME/.nix-profile/lib/vst3:/run/current-system/sw/lib/vst3";
   };
