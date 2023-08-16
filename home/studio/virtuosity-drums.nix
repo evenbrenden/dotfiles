1e5838cd9a902d4f8b315a794f5ccf8f3e916a8f
Even Brenden <evenbrenden@users.noreply.github.com>
2023-08-16 19:30:05 +0200

    Revert "studio: Link some resources to home directory"
    This reverts commit f2b396997be7a189d35fcfdee10167fe4a943529.

---
+++ home/studio/virtuosity-drums.nix
@@ -0,0 +1,18 @@
+{ pkgs }:
+
+pkgs.stdenv.mkDerivation {
+  pname = "virtuosity-drums";
+  version = "0.924";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "sfzinstruments";
+    repo = "virtuosity_drums";
+    rev = "v0.924";
+    sha256 = "sha256-kedn/auowOdejv0CE97Xvc3t7tkaIqnYEk0KtNO+Md4=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/share/studio/instruments/virtuosity-drums
+    cp -r * $out/share/studio/instruments/virtuosity-drums
+  '';
+}
