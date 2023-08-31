a4d0d474fa1d1162d1b571c6571d63488a2db447
Even Brenden <evenbrenden@users.noreply.github.com>
2023-08-31 19:09:16 +0200

    i3: Use rfkill to toggle WLAN

--- home/i3/i3.nix
+++ home/i3/i3.nix
@@ -11,7 +11,6 @@
     (import ./set-dpi.nix { inherit pkgs; })
     tesseract
     (import ./toggle-keyboard-layout.nix { inherit pkgs; })
-    (import ./toggle-wifi.nix { inherit pkgs; })
     xcwd
     xrandr-invert-colors
   ];
