e5377cabadd4e1d4512e9826792e39e8f20c0310
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-10 13:56:01 +0100

    ...with a wallpaper to match

--- home/refresh-wallpaper.nix
+++ home/refresh-wallpaper.nix
@@ -4,6 +4,6 @@ pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -cover ${./henri-matisse-les-codomas.jpeg}
+    hsetroot -cover ${./henri-matisse-the-swimmer-in-the-tank.jpeg}
   '';
 }
