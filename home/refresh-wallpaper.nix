0394ad88981af7d81e9b5d13c7ecc2d4fe2aa4bb
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-16 19:47:00 +0100

    Nothing wall

--- home/refresh-wallpaper.nix
+++ home/refresh-wallpaper.nix
@@ -1,13 +1,9 @@
 { pkgs }:
-let
-  awesome_bg_0 = builtins.fetchurl {
-    url = "https://trisquel.info/files/awesome_bg_0.png";
-    sha256 = "04jszqp8g63s7dm6k07j8plq4dx5c3cv6rp3xzx5ihxhl433jc9k";
-  };
-in pkgs.writeShellApplication {
+
+pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -alpha 64 -tile ${awesome_bg_0}
+    hsetroot -solid '#000000'
   '';
 }
