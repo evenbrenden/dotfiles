d05a93b7e2d68effe1e1ac7a0cccb4f931d70b2a
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-16 20:56:04 +0100

    Revert "Nothing wall"
    This reverts commit 0394ad88981af7d81e9b5d13c7ecc2d4fe2aa4bb.

--- home/refresh-wallpaper.nix
+++ home/refresh-wallpaper.nix
@@ -1,9 +1,13 @@
 { pkgs }:
-
-pkgs.writeShellApplication {
+let
+  awesome_bg_0 = builtins.fetchurl {
+    url = "https://trisquel.info/files/awesome_bg_0.png";
+    sha256 = "04jszqp8g63s7dm6k07j8plq4dx5c3cv6rp3xzx5ihxhl433jc9k";
+  };
+in pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -solid '#000000'
+    hsetroot -alpha 64 -tile ${awesome_bg_0}
   '';
 }
