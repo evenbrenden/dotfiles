84a3864ccca6d69356ef1706f501aea796bbd0dd
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-17 18:09:53 +0100

    Add comment

--- home/refresh-wallpaper.nix
+++ home/refresh-wallpaper.nix
@@ -1,5 +1,6 @@
 { pkgs }:
 let
+  # https://trisquel.info/en/forum/old-fashioned-tiled-wallpaper
   awesome_bg_0 = builtins.fetchurl {
     url = "https://trisquel.info/files/awesome_bg_0.png";
     sha256 = "04jszqp8g63s7dm6k07j8plq4dx5c3cv6rp3xzx5ihxhl433jc9k";
