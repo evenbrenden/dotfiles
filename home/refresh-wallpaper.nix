d2c918d1b7da6d341cd33a4ca90b8a3cdf709e56
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-22 11:24:12 +0100

    Sample

--- home/refresh-wallpaper.nix
+++ home/refresh-wallpaper.nix
@@ -1,9 +1,10 @@
 { pkgs }:
 
+# https://www.discogs.com/master/470489-Laurie-Spiegel-The-Expanding-Universe
 pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -solid '#000000'
+    hsetroot -add '#DB3796' -add '#FAD905' -gradient 0
   '';
 }
