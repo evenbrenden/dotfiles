f94cd41da1dab5d10ee452bcb83bb529e729aee5
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-22 11:19:24 +0100

    Give up

--- home/refresh-wallpaper.nix
+++ home/refresh-wallpaper.nix
@@ -4,6 +4,6 @@ pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -alpha 128 -solid '#8B104E'
+    hsetroot -solid '#000000'
   '';
 }
