b88ee6b306ee23be4231de9611aebac15841e579
Even Brenden <evenbrenden@users.noreply.github.com>
2023-11-22 10:17:56 +0100

    Dark dark pink

--- home/refresh-wallpaper.nix
+++ home/refresh-wallpaper.nix
@@ -1,10 +1,9 @@
 { pkgs }:
 
-# Tetradic colors #104E8B #8B108B #8B4D10 #118B10
 pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -add '#8B108B' -add '#8B4D10' -gradient 0
+    hsetroot -alpha 128 -solid '#8B104E'
   '';
 }
