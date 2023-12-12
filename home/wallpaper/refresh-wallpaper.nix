6376121f14d4b271393c2f8fa63157af85ad7d94
Even Brenden <evenbrenden@users.noreply.github.com>
2023-12-12 19:39:28 +0100

    Narcissist

--- home/wallpaper/refresh-wallpaper.nix
+++ home/wallpaper/refresh-wallpaper.nix
@@ -1,10 +1,9 @@
 { pkgs }:
 
-# https://www.discogs.com/master/470489-Laurie-Spiegel-The-Expanding-Universe
 pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -add '#DB3796' -add '#FAD905' -gradient 0
+    hsetroot -tile ${./stars.jpeg} -center ${./andy-dixon-narcissus.jpeg}
   '';
 }
