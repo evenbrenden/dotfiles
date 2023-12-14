0e7a2809b51755b578346c00d39425c40645ffe9
Even Brenden <evenbrenden@users.noreply.github.com>
2023-12-14 10:27:32 +0100

    Math Is ... Universal

--- home/wallpaper/refresh-wallpaper.nix
+++ home/wallpaper/refresh-wallpaper.nix
@@ -1,10 +1,28 @@
 { pkgs }:
 
-# https://www.discogs.com/master/470489-Laurie-Spiegel-The-Expanding-Universe
-pkgs.writeShellApplication {
+let
+  math-is-universal = pkgs.stdenv.mkDerivation {
+    name = "math-is-universal";
+    src = pkgs.fetchurl {
+      url = "https://i.imgur.com/FYxA2.jpeg";
+      sha256 = "sha256-+dWII8TNDqWXkgAkSCqSFM/Sppu4JpEyFZrTDYxkdDo=";
+    };
+    unpackPhase = ''
+      cp -a $src input.jpeg
+    '';
+    buildInputs = [ pkgs.imagemagick ];
+    buildPhase = ''
+      convert -bordercolor '#37201C' -border 84 -resize 50% input.jpeg output.jpeg
+    '';
+    installPhase = ''
+      mkdir -p $out
+      cp -a output.jpeg $out/math-is-universal.jpeg
+    '';
+  };
+in pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -add '#DB3796' -add '#FAD905' -gradient 0
+    hsetroot -solid '#265F64' -center ${math-is-universal}/math-is-universal.jpeg
   '';
 }
