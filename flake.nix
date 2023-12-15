07bcc428043133efca5a2ba40ae448ac760ec499
Even Brenden <evenbrenden@users.noreply.github.com>
2023-12-15 20:53:23 +0100

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -17,61 +17,51 @@
 
   outputs = { nixpkgs-stable, nixpkgs-unstable, home-manager, musnix, i3quo, attic, ... }:
     let
-      system = "x86_64-linux";
-      # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/3
-      pkgs = import nixpkgs-stable {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = common-overlays;
-      };
-      overlay-unstable = final: prev: {
-        unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      };
-      common-overlays = [
-        i3quo.overlay
-        overlay-unstable
-        attic.overlays.default
-        # https://github.com/NixOS/nixpkgs/pull/182069#issuecomment-1213432500
-        (self: super: {
-          firefox = super.firefox.overrideAttrs
-            (old: { libs = old.libs + ":" + pkgs.lib.makeLibraryPath [ pkgs.nss_latest ]; });
-        })
-      ];
-      # https://discourse.nixos.org/t/nix-run-refuses-to-evaluate-unfree-packages-even-though-allowunfree-true-in-my-config/13653/5
-      nix-settings = {
+      nix-config-module = {
         nix = {
+          # https://discourse.nixos.org/t/nix-run-refuses-to-evaluate-unfree-packages-even-though-allowunfree-true-in-my-config/13653/5
           nixPath = [ "nixpkgs=${nixpkgs-unstable}" ];
           registry.nixpkgs.flake = nixpkgs-unstable;
         };
       };
+      nixpkgs-overlays-module = {
+        nixpkgs.overlays = [
+          (final: prev: {
+            # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/3
+            unstable = import nixpkgs-unstable {
+              inherit system;
+              config.allowUnfree = true;
+            };
+            # https://github.com/NixOS/nixpkgs/pull/182069#issuecomment-1213432500
+            firefox = prev.firefox.overrideAttrs
+              (old: { libs = old.libs + ":" + prev.lib.makeLibraryPath [ prev.nss_latest ]; });
+          })
+          i3quo.overlay
+          attic.overlays.default
+        ];
+      };
+      home-manager-config-module = {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.evenbrenden = import ./home/home.nix;
+      };
+      common-modules =
+        [ nix-config-module nixpkgs-overlays-module home-manager.nixosModules.home-manager home-manager-config-module ];
+      system = "x86_64-linux";
     in {
       # sudo -EH nixos-rebuild switch --flake .#[configuration]
-      nixosConfigurations = let
-        commonModules = [
-          nix-settings
-          { nixpkgs.overlays = common-overlays; }
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.evenbrenden = import ./home/home.nix;
-          }
-        ];
-      in {
+      nixosConfigurations = {
         gaucho = nixpkgs-stable.lib.nixosSystem {
           inherit system;
-          modules = commonModules ++ [ ./nixos/gaucho/configuration.nix musnix.nixosModules.musnix ];
+          modules = common-modules ++ [ ./nixos/gaucho/configuration.nix musnix.nixosModules.musnix ];
         };
         naxos = nixpkgs-stable.lib.nixosSystem {
           inherit system;
-          modules = commonModules ++ [ ./nixos/naxos/configuration.nix musnix.nixosModules.musnix ];
+          modules = common-modules ++ [ ./nixos/naxos/configuration.nix musnix.nixosModules.musnix ];
         };
         work = nixpkgs-stable.lib.nixosSystem {
           inherit system;
-          modules = commonModules ++ [ ./nixos/work/configuration.nix ];
+          modules = common-modules ++ [ ./nixos/work/configuration.nix ];
         };
       };
     };
