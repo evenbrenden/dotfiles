f9dc352d4f38dae70129a972153878b865f162d9
Even Brenden <evenbrenden@users.noreply.github.com>
2023-12-25 15:37:16 +0100

    That was a bit much

--- home/wallpaper/refresh-wallpaper.nix
+++ home/wallpaper/refresh-wallpaper.nix
@@ -1,10 +1,6 @@
 { pkgs }:
 
 let
-  narcissus = pkgs.fetchurl {
-    url = "https://elemmental.com/wp-content/uploads/2018/12/Andy-Dixon-Narcissus-Painting-139-X-80-2018.jpg";
-    sha256 = "sha256-NbDYibYjSq3u2z7206H2Rl4W/DOcKLuLhz00PEMgwbo=";
-  };
   the-witchs-broom = pkgs.fetchurl {
     url = "https://jimstar11.com/NGC6960_Bicolor4.jpg";
     sha256 = "sha256-eZbTyC1JzE7kQ0ngNRYjrznOLeCCBlcXsFq0omJ8C3s=";
@@ -13,6 +9,6 @@ in pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -cover ${the-witchs-broom} -center ${narcissus}
+    hsetroot -cover ${the-witchs-broom}
   '';
 }
