5023c0d92beeaa6ab3c8437bc8cef7ce99919fbb
Even Brenden <evenbrenden@users.noreply.github.com>
2024-03-25 12:43:10 +0100

    Landscape drawing

--- home/wallpaper/refresh-wallpaper.nix
+++ home/wallpaper/refresh-wallpaper.nix
@@ -1,14 +1,14 @@
 { pkgs }:
 
 let
-  the-witchs-broom = pkgs.fetchurl {
-    url = "https://jimstar11.com/NGC6960_Bicolor4.jpg";
-    sha256 = "sha256-eZbTyC1JzE7kQ0ngNRYjrznOLeCCBlcXsFq0omJ8C3s=";
+  landscape-drawing = pkgs.fetchurl {
+    url = "https://i.etsystatic.com/27770216/r/il/5eb29a/3176801816/il_fullxfull.3176801816_r95l.jpg";
+    sha256 = "sha256-WPpbBW7NwxBOKg93ofqOfEOprD1SvxChKZvzkcTqHC8=";
   };
 in pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -cover ${the-witchs-broom}
+    hsetroot -fill ${landscape-drawing}
   '';
 }
