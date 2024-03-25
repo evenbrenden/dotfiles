caa47228bbfcc5cd89522ad6a88fe8d699b3c04a
Even Brenden <evenbrenden@users.noreply.github.com>
2024-03-25 15:42:52 +0100

    Revert "Rose quartz"
    This reverts commit 146e4eed870696ece59e122efdee060cdd9e808d.

--- home/wallpaper/refresh-wallpaper.nix
+++ home/wallpaper/refresh-wallpaper.nix
@@ -1,9 +1,14 @@
 { pkgs }:
 
-pkgs.writeShellApplication {
+let
+  landscape-drawing = pkgs.fetchurl {
+    url = "https://i.etsystatic.com/27770216/r/il/5eb29a/3176801816/il_fullxfull.3176801816_r95l.jpg";
+    sha256 = "sha256-WPpbBW7NwxBOKg93ofqOfEOprD1SvxChKZvzkcTqHC8=";
+  };
+in pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
-    hsetroot -solid '#AA98A9' # Rose quartz
+    hsetroot -fill ${landscape-drawing}
   '';
 }
