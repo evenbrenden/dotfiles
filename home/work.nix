05bb5c826a1ef1a86aea8b820b10102a88583a60
Even Brenden <evenbrenden@users.noreply.github.com>
2024-05-06 12:48:02 +0200

    Refactor

---
+++ home/work.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    attic-client
+    avro-tools
+    dbeaver
+    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.gke-gcloud-auth-plugin ])
+    jdk
+    jetbrains.idea-community
+    k9s
+    kubectl
+    nodejs
+    nodePackages.pnpm
+    sbt
+    yarn
+    xmlformat
+  ];
+}
