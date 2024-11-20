5d407e7d0c0dfc98efd6e2fd865f0f08424856c8
Even Brenden <evenbrenden@users.noreply.github.com>
2024-11-20 10:12:03 +0100

    Update nixpkgs-unstable and remove signal-desktop overlay

--- home/signal-desktop.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }:
-
-pkgs.signal-desktop.overrideAttrs (_: rec {
-  version = "7.16.0";
-  src = pkgs.fetchurl {
-    url = "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-    hash = "sha256-DfPQb3TGhVVZ7webNoMmyhjhRKKO3lWf12ZIpi7D7tc=";
-  };
-})
