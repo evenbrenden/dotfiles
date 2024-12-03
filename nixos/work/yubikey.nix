c5c18551ac90bac1bb9967e321f1d16755e6526a
Even Brenden <evenbrenden@users.noreply.github.com>
2024-12-03 13:25:00 +0100

    Update nixpkgs-stable

--- nixos/work/yubikey.nix
+++ nixos/work/yubikey.nix
@@ -1,9 +1,7 @@
 # https://nixos.wiki/wiki/Yubikey
 
 { pkgs, ... }: {
-
-  # https://github.com/NixOS/nixpkgs/issues/353664
-  # environment.systemPackages = [ pkgs.yubioath-flutter ];
+  environment.systemPackages = [ pkgs.yubioath-flutter ];
 
   security.pam.services = {
     login.u2fAuth = true;
