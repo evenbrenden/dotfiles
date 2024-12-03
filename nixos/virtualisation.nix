fea826bb26edbfde1d0c0ea3aed35f56cde939d6
Even Brenden <evenbrenden@users.noreply.github.com>
2024-12-03 16:50:43 +0100

    Use QEMU/KVM/virt-manager

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -4,11 +4,19 @@
   # Docker
   virtualisation.docker.enable = true;
 
-  # Vagrant
-  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
-  # https://github.com/NixOS/nixpkgs/issues/348938
-  # environment.systemPackages = [ pkgs.vagrant ];
+  # libvirt
   virtualisation.libvirtd.enable = true;
+  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
+  programs.virt-manager = {
+    enable = true;
+    # https://github.com/NixOS/nixpkgs/issues/267579#issuecomment-1815519722
+    package = with pkgs;
+      (virt-manager.overrideAttrs (old: {
+        nativeBuildInputs = old.nativeBuildInputs ++ [ wrapGAppsHook ];
+        buildInputs = lib.lists.subtractLists [ wrapGAppsHook ] old.buildInputs
+          ++ [ gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good ];
+      }));
+  };
 
   # Groups
   users.users.${username}.extraGroups = [ "docker" "libvirtd" ];
