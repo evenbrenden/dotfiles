68bff82b614a034ca550bce1c2b6439549ef74c8
Even Brenden <evenbrenden@users.noreply.github.com>
2024-12-03 16:50:52 +0100

    Revert "Use QEMU/KVM/virt-manager"
    This reverts commit 672cdd9ed2406c1c9d9b7bfce80aea84c0722573.

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -4,19 +4,11 @@
   # Docker
   virtualisation.docker.enable = true;
 
-  # libvirt
-  virtualisation.libvirtd.enable = true;
+  # Vagrant
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
-  programs.virt-manager = {
-    enable = true;
-    # https://github.com/NixOS/nixpkgs/issues/267579#issuecomment-1815519722
-    package = with pkgs;
-      (virt-manager.overrideAttrs (old: {
-        nativeBuildInputs = old.nativeBuildInputs ++ [ wrapGAppsHook ];
-        buildInputs = lib.lists.subtractLists [ wrapGAppsHook ] old.buildInputs
-          ++ [ gst_all_1.gst-plugins-base gst_all_1.gst-plugins-good ];
-      }));
-  };
+  # https://github.com/NixOS/nixpkgs/issues/348938
+  # environment.systemPackages = [ pkgs.vagrant ];
+  virtualisation.libvirtd.enable = true;
 
   # Groups
   users.users.${username}.extraGroups = [ "docker" "libvirtd" ];
