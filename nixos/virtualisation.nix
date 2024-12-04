adc071c63ba60a3ee73f673c17cf4d96aa3fe6a2
Even Brenden <evenbrenden@users.noreply.github.com>
2024-12-04 10:44:40 +0100

    Back to VirtualBox

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,15 +1,18 @@
 { pkgs, username }:
 
 {
-  # Docker
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    # Docker
+    docker.enable = true;
 
-  # Vagrant
-  boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
-  # https://github.com/NixOS/nixpkgs/issues/348938
-  # environment.systemPackages = [ pkgs.vagrant ];
-  virtualisation.libvirtd.enable = true;
+    # VirtualBox
+    virtualbox.host = {
+      enable = true;
+      # https://discourse.nixos.org/t/virtualbox-keeps-getting-rebuilt/6612
+      enableExtensionPack = true;
+    };
+  };
 
   # Groups
-  users.users.${username}.extraGroups = [ "docker" "libvirtd" ];
+  users.users.${username}.extraGroups = [ "docker" "vboxusers" ];
 }
