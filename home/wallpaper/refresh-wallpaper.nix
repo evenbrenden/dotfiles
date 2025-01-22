0c8035313aafbb751426e24cc7667bde4646c5d4
Even Brenden <evenbrenden@users.noreply.github.com>
2025-01-22 19:47:23 +0100

    Generate wallpapers myself

--- home/wallpaper/refresh-wallpaper.nix
+++ home/wallpaper/refresh-wallpaper.nix
@@ -1,14 +1,31 @@
 { pkgs }:
 
-pkgs.writeShellApplication {
+let
+  # https://github.com/himdel/hsetroot/issues/42
+  the-expanding-universe = pkgs.stdenv.mkDerivation {
+    name = "the-expanding-universe";
+    src = null;
+    unpackPhase = "true";
+    nativeBuildInputs = [ pkgs.imagemagick ];
+    buildPhase = ''
+      magick -size 5120x2160 -define gradient:angle=30 gradient:#FDE507-#FA2AB4 the-expanding-universe-a.png
+      magick -size 5120x2160 -define gradient:angle=30 gradient:#D00E91-#069AFC the-expanding-universe-b.png
+    '';
+    installPhase = ''
+      mkdir -p $out/share/wallpapers
+      cp the-expanding-universe-a.png $out/share/wallpapers/the-expanding-universe-a.png
+      cp the-expanding-universe-b.png $out/share/wallpapers/the-expanding-universe-b.png
+    '';
+  };
+in pkgs.writeShellApplication {
   name = "refresh-wallpaper";
   runtimeInputs = with pkgs; [ hsetroot ];
   text = ''
     current_hour=$(date +%H)
     if [ "$current_hour" -ge 6 ] && [ "$current_hour" -lt 18 ]; then
-      hsetroot -add '#FA2AB4' -add '#FDE507' -gradient 30
+      hsetroot -fill ${the-expanding-universe}/share/wallpapers/the-expanding-universe-a.png
     else
-      hsetroot -add '#069AFC' -add '#D00E91' -gradient 30
+      hsetroot -fill ${the-expanding-universe}/share/wallpapers/the-expanding-universe-b.png
     fi
   '';
 }
