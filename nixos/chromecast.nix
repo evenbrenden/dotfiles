a0569f9142eedb537fdc2e778add43c4ddb06fc5
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-01 20:28:02 +0100

    Disable avahi
    Restructure.

---
+++ nixos/chromecast.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  # https://github.com/NixOS/nixpkgs/issues/49630
+  # services.avahi.enable = true;
+  # systemctl stop firewall.service
+  # nix run nixpkgs#chromium -- --load-media-router-component-extension=1
+}
