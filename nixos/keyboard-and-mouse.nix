5f924a2b3fd8ae3fcd66e4bad0af7298c44388a4
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-02 10:29:41 +0100

    Move .Xresources home

--- nixos/keyboard-and-mouse.nix
+++ nixos/keyboard-and-mouse.nix
@@ -7,27 +7,11 @@
       touchpad.tapping = true;
     };
 
-    xserver = {
-      # This can be moved to home-manager
-      displayManager.sessionCommands = let
-        xresources = pkgs.writeText "Xresources" ''
-          Xcursor.size: 48
-          Xcursor.theme: Adwaita
-          XTerm*faceName: DejaVu Sans Mono
-          XTerm*faceSize: 12
-        '';
-      in ''
-        ${pkgs.xorg.xrdb}/bin/xrdb -merge <${xresources}
-      '';
-
-      xkb = {
-        extraLayouts.norwerty = let norwerty = import ./norwerty/norwerty.nix { inherit pkgs; };
-        in {
-          description = "Norwerty";
-          languages = [ "no" ];
-          symbolsFile = "${norwerty}/share/X11/xkb/symbols/norwerty";
-        };
-      };
+    xserver.xkb.extraLayouts.norwerty = let norwerty = import ./norwerty/norwerty.nix { inherit pkgs; };
+    in {
+      description = "Norwerty";
+      languages = [ "no" ];
+      symbolsFile = "${norwerty}/share/X11/xkb/symbols/norwerty";
     };
   };
 }
