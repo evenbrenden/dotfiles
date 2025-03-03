97b99ee30fdfb7ef18fdf15ad66167e8b612afde
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-03 14:27:10 +0100

    Add comment

--- nixos/display.nix
+++ nixos/display.nix
@@ -10,6 +10,7 @@
       enable = true;
 
       # This can be moved to home-manager
+      # https://discourse.nixos.org/t/opening-i3-from-home-manager-automatically/4849/8
       windowManager.i3.enable = true;
     };
   };
