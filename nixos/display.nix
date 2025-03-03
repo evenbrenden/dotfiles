93e6cc3a2a62a98e74fcc18c3b39814d8b581367
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-03 10:56:15 +0100

    Move Picom service home

--- nixos/display.nix
+++ nixos/display.nix
@@ -4,15 +4,6 @@
   services = {
 
     displayManager.defaultSession = "none+i3";
-
-    # This can be moved to home-manager
-    picom = {
-      backend = "xr_glx_hybrid"; # For xsecurelock
-      enable = true;
-      fade = false; # https://github.com/google/xsecurelock/issues/97#issuecomment-1100903794
-      vSync = true; # Combat screen tearing
-    };
-
     xserver = {
       # Combat screen tearing
       deviceSection = ''
