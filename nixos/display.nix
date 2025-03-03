1b8fab1aa6eff9fa4b2f0de2f706c0c76411c965
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-03 13:03:41 +0100

    Tidy up

--- nixos/display.nix
+++ nixos/display.nix
@@ -2,14 +2,12 @@
 
 {
   services = {
-
     displayManager.defaultSession = "none+i3";
     xserver = {
       # Combat screen tearing
       deviceSection = ''
         Option "TearFree" "true"
       '';
-
       enable = true;
 
       # This can be moved to home-manager
