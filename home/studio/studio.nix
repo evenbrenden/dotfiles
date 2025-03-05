43fbee835a04d8dede075ce2be3dd858b183cb1c
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-05 12:33:41 +0100

    Clean up studio

--- home/studio/studio.nix
+++ home/studio/studio.nix
@@ -2,6 +2,16 @@
 
 {
   home = {
+    file = {
+      "studio/ir".source =
+        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/ir";
+      "studio/midi".source =
+        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/midi";
+      "studio/sfz".source =
+        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/sfz";
+      "studio/soundfonts".source =
+        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/soundfonts";
+    };
     packages = with pkgs;
       let
         locals = [
@@ -26,19 +36,6 @@
           (import ./yamaha-tx81z-lately-bass.nix { inherit pkgs; })
         ];
       in locals ++ remotes;
-
-    # For easy access
-    file = {
-      "studio/ir".source =
-        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/ir";
-      "studio/midi".source =
-        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/midi";
-      "studio/sfz".source =
-        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/sfz";
-      "studio/soundfonts".source =
-        config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/soundfonts";
-    };
-
     sessionVariables = {
       LV2_PATH = "/etc/profiles/per-user/${config.home.username}/lib/lv2";
       UHE_RT_PRIO = 64; # For u-he plugins
