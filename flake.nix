c77808cb6fa7fe94f22904bf90cbce26a8159502
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-05 14:09:52 +0100

    Tidy up root Nix files

--- flake.nix
+++ flake.nix
@@ -2,42 +2,42 @@
   description = "evenbrenden/dotfiles";
 
   inputs = {
-    nixpkgs-stable.url = "nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
+    i3quo.url = "git+https://codeberg.org/evenbrenden/i3quo";
+    i3quo.inputs.nixpkgs.follows = "nixpkgs-stable";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
     musnix.url = "github:musnix/musnix";
     musnix.inputs.nixpkgs.follows = "nixpkgs-stable";
-    i3quo.url = "git+https://codeberg.org/evenbrenden/i3quo";
-    i3quo.inputs.nixpkgs.follows = "nixpkgs-stable";
+    nixpkgs-stable.url = "nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "nixpkgs/nixpkgs-unstable";
     sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = { nixpkgs-stable, nixpkgs-unstable, home-manager, musnix, i3quo, sops-nix, ... }:
+  outputs = { i3quo, home-manager, musnix, nixpkgs-stable, nixpkgs-unstable, sops-nix, ... }:
     let
+      common-modules = [
+        home-manager-config-module
+        home-manager.nixosModules.home-manager
+        (import ./nix.nix { inherit nixpkgs-stable nixpkgs-unstable; })
+        (import ./nixpkgs.nix { inherit system nixpkgs-unstable i3quo; })
+      ];
       home-manager-config-module.home-manager = {
+        backupFileExtension = "backup";
+        extraSpecialArgs = { sops-nix = sops-nix.homeManagerModules.sops; };
         useGlobalPkgs = true;
         useUserPackages = true;
         users.evenbrenden = import ./home/home.nix;
-        extraSpecialArgs = { sops-nix = sops-nix.homeManagerModules.sops; };
-        backupFileExtension = "backup";
       };
-      common-modules = [
-        (import ./nix.nix { inherit nixpkgs-stable nixpkgs-unstable; })
-        (import ./nixpkgs.nix { inherit system nixpkgs-unstable i3quo; })
-        home-manager.nixosModules.home-manager
-        home-manager-config-module
-      ];
       system = "x86_64-linux";
     in {
       nixosConfigurations = {
         naxos = nixpkgs-stable.lib.nixosSystem {
-          inherit system;
           modules = common-modules ++ [ ./nixos/naxos/configuration.nix musnix.nixosModules.musnix ];
+          inherit system;
         };
         work = nixpkgs-stable.lib.nixosSystem {
-          inherit system;
           modules = common-modules ++ [ ./nixos/work/configuration.nix ];
+          inherit system;
         };
       };
     };
