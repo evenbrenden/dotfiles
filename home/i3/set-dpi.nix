a12b5981c97ce461debd3f94f53ed742e9e92ef4
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-05 11:20:43 +0100

    i3: Tighten up

--- home/i3/set-dpi.nix
+++ home/i3/set-dpi.nix
@@ -2,6 +2,7 @@
 
 pkgs.writeShellApplication {
   name = "set-dpi";
+  runtimeInputs = with pkgs.xorg; [ xrandr xrdb ];
   text = ''
     xrandr --dpi "$1" && echo "Xft.dpi: $1" | xrdb -merge
   '';
