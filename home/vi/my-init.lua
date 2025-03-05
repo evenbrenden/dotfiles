97fd99fce3c950c4f2dd49d2f7d11f9c953f5a8c
Your Name <you@example.com>
2025-03-05 20:09:30 +0100

    Wrap nixfmt-classic
    Add nixfmt-all.

--- home/vi/my-init.lua
+++ home/vi/my-init.lua
@@ -115,7 +115,7 @@ vim.g.neoformat_haskell_fourmolu = {
 }
 vim.g.neoformat_enabled_lua = {'luaformat'}
 vim.g.neoformat_enabled_nix = {'nixfmt'}
-vim.g.neoformat_nix_nixfmt = {exe = 'nixfmt', args = {'--width=120'}, stdin = 1}
+vim.g.neoformat_nix_nixfmt = {exe = 'nixfmt', stdin = 1}
 vim.g.neoformat_enabled_scala = {'scalafmt'}
 vim.g.neoformat_enabled_sh = {'shfmt'}
 vim.g.neoformat_sh_shfmt = {
