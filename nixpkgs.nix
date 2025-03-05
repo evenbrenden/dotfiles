fde3ec02c7fd10370abc289701980522f197c35e
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-05 14:09:52 +0100

    Tidy up root Nix files

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -10,10 +10,10 @@ in {
     inherit config;
     overlays = [
       (final: prev: {
+        sof-firmware = with prev; import ./nixos/naxos/sof-firmware.nix { inherit fetchurl lib stdenvNoCC; };
         unstable = import nixpkgs-unstable {
           inherit system config;
         }; # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/3
-        sof-firmware = with prev; import ./nixos/naxos/sof-firmware.nix { inherit fetchurl lib stdenvNoCC; };
       })
       i3quo.overlay
     ];
