212de739847cfd224d0fd6854b1b50d5f074447a
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-05 11:14:21 +0100

    Type set-pulseaudio-sink-port.py

--- nixos/naxos/set-pulseaudio-sink-port.py
+++ nixos/naxos/set-pulseaudio-sink-port.py
@@ -2,21 +2,21 @@
 
 import subprocess
 
-sink_name = 'alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0001.hw_sofhdadsp__sink'
-headphones_name = 'Headphones'
-speaker_name = 'Speaker'
+sink_name: str = 'alsa_output.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi___ucm0001.hw_sofhdadsp__sink'
+headphones_name: str = 'Headphones'
+speaker_name: str = 'Speaker'
 
 output = subprocess.run(['pactl', 'list', 'sinks'], stdout=subprocess.PIPE)
-verbose = output.stdout.decode()
-sink = verbose.partition(sink_name)[2]
-ports = sink.partition('Ports')[2]
-headphones = ports.partition(headphones_name)[2]
-closed = headphones.splitlines()[0]
-available = 'not available' not in closed
+verbose: str = output.stdout.decode()
+sink: str = verbose.partition(sink_name)[2]
+ports: str = sink.partition('Ports')[2]
+headphones: str = ports.partition(headphones_name)[2]
+closed: str = headphones.splitlines()[0]
+available: bool = 'not available' not in closed
 
 if available:
-    jiggle_port = speaker_name
-    target_port = headphones_name
+    jiggle_port: str = speaker_name
+    target_port: str = headphones_name
 else:
     jiggle_port = headphones_name
     target_port = speaker_name
