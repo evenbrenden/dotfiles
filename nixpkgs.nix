c8365207d962cba2e43138215bc8ec08dc3d3802
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-06 10:49:28 +0100

    Rename nixfmt-classic.nix

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -10,7 +10,7 @@ in {
     inherit config;
     overlays = [
       (final: prev: {
-        nixfmt-classic = import home/nixfmt.nix { pkgs = prev; };
+        nixfmt-classic = import home/nixfmt-classic.nix { pkgs = prev; };
         sof-firmware = with prev; import ./nixos/naxos/sof-firmware.nix { inherit fetchurl lib stdenvNoCC; };
         # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/3
         unstable = import nixpkgs-unstable { inherit system config; };
