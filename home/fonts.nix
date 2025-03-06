c6e89abe51a4ae31178eae215cd17a1e88c12656
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-06 08:34:16 +0100

    Move comment

--- home/fonts.nix
+++ home/fonts.nix
@@ -3,6 +3,7 @@
 {
   fonts.fontconfig.enable = true;
   home.packages = [ pkgs.dejavu_fonts ];
+  # Fixes some blocky fonts in Firefox
   xdg.configFile."fontconfig/conf.d/70-no-bitmaps.conf".text = ''
     <?xml version="1.0"?>
     <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
@@ -16,5 +17,5 @@
         </rejectfont>
       </selectfont>
     </fontconfig>
-  ''; # Fixes some blocky fonts in Firefox
+  '';
 }
