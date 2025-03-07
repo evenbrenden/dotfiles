dafb058fb8884a639cf658f79f740ad55dcb05d7
Even Brenden <2512008+evenbrenden@users.noreply.github.com>
2025-03-07 08:38:30 +0100

    Consolidate Nixpkgs (#8)

--- home/studio.nix
+++ home/studio.nix
@@ -12,30 +12,25 @@
       "studio/soundfonts".source =
         config.lib.file.mkOutOfStoreSymlink "/etc/profiles/per-user/${config.home.username}/share/soundfonts";
     };
-    packages = with pkgs;
-      let
-        locals = [
-          (import ./bolder-sounds.nix { inherit pkgs; })
-          (import ./digital-sound-factory.nix { inherit pkgs; })
-          (import ./ivy-audio.nix { inherit pkgs; })
-          (import ./samples-from-mars.nix { inherit pkgs; })
-        ];
-        remotes = [
-          (import ./ac-upright.nix { inherit pkgs; })
-          carla
-          (import ./dsmolken-double-bass.nix { inherit pkgs; })
-          (import ./fretls-dry.nix { inherit pkgs; })
-          (import ./instant-midi-drum-patterns.nix { inherit pkgs; })
-          (import ./jsteeldrum.nix { inherit pkgs; })
-          (import ./lexicon-lxp-1-impulse-responses.nix { inherit pkgs; })
-          reaper
-          sfizz
-          soundfont-fluid
-          (import ./virtuosity-drums.nix { inherit pkgs; })
-          (import ./wet-fretls.nix { inherit pkgs; })
-          (import ./yamaha-tx81z-lately-bass.nix { inherit pkgs; })
-        ];
-      in locals ++ remotes;
+    packages = with pkgs; [
+      ac-upright
+      bolder-sounds
+      carla
+      digital-sound-factory
+      dsmolken-double-bass
+      fretls-dry
+      instant-midi-drum-patterns
+      ivy-audio
+      jsteeldrum
+      lexicon-lxp-1-impulse-responses
+      reaper
+      samples-from-mars
+      sfizz
+      soundfont-fluid
+      virtuosity-drums
+      wet-fretls
+      yamaha-tx81z-lately-bass
+    ];
     sessionVariables = {
       LV2_PATH = "/etc/profiles/per-user/${config.home.username}/lib/lv2";
       UHE_RT_PRIO = 64; # For u-he plugins
