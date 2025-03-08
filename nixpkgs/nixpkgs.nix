28636d1fb572fe73a392e00b6906fe69d41d49ca
Your Name <you@example.com>
2025-03-08 11:44:58 +0100

    Remove comment

--- nixpkgs/nixpkgs.nix
+++ nixpkgs/nixpkgs.nix
@@ -19,7 +19,6 @@ in {
         set-dpi = import ./set-dpi.nix { pkgs = prev; };
         sof-firmware = with prev; import ./sof-firmware.nix { inherit fetchurl lib stdenvNoCC; };
         toggle-keyboard-layout = import ./toggle-keyboard-layout.nix { pkgs = prev; };
-        # https://discourse.nixos.org/t/using-nixpkgs-legacypackages-system-vs-import/17462/3
         unstable = import nixpkgs-unstable { inherit system config; };
         x-www-browser = import ./x-www-browser.nix { pkgs = prev; };
       })
