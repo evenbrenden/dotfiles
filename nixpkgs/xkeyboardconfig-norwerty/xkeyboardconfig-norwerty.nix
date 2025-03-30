4b4da4f2b36fb2f7da23ce35659170805e990c5b
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-30 20:01:24 +0200

    xkeyboardconfig-norwerty: Add automake to nativeBuildInputs

--- nixpkgs/xkeyboardconfig-norwerty/xkeyboardconfig-norwerty.nix
+++ nixpkgs/xkeyboardconfig-norwerty/xkeyboardconfig-norwerty.nix
@@ -1,4 +1,7 @@
 { pkgs }:
 
-pkgs.xorg.xkeyboardconfig.overrideAttrs
-(_: previousAttrs: { patches = (previousAttrs.patches or [ ]) ++ [ ./xkeyboardconfig-norwerty.patch ]; })
+pkgs.xorg.xkeyboardconfig.overrideAttrs (_: previousAttrs: {
+  nativeBuildInputs = previousAttrs.nativeBuildInputs
+    ++ [ pkgs.automake ]; # Because xorg.xkeyboardconfig_custom does it
+  patches = (previousAttrs.patches or [ ]) ++ [ ./xkeyboardconfig-norwerty.patch ];
+})
