4eddd9c9a7b3ecef4f2f6f8f5adbc74876a8ee4c
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-31 08:46:04 +0200

    xkeyboardconfig-norwerty: Remove automake from nativeBuildInputs
    Doesn't seem to make a difference.

--- nixpkgs/xkeyboardconfig-norwerty/xkeyboardconfig-norwerty.nix
+++ nixpkgs/xkeyboardconfig-norwerty/xkeyboardconfig-norwerty.nix
@@ -1,6 +1,4 @@
 { pkgs }:
 
-pkgs.xorg.xkeyboardconfig.overrideAttrs (prevAttrs: {
-  nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.automake ]; # Because xorg.xkeyboardconfig_custom does it
-  patches = (prevAttrs.patches or [ ]) ++ [ ./xkeyboardconfig-norwerty.patch ];
-})
+pkgs.xorg.xkeyboardconfig.overrideAttrs
+(prevAttrs: { patches = (prevAttrs.patches or [ ]) ++ [ ./xkeyboardconfig-norwerty.patch ]; })
