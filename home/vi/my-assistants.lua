cf828d71761691048a25490aa6209ae650061b38
Even Brenden <evenbrenden@users.noreply.github.com>
2025-03-31 09:34:18 +0200

    copilot-vim: Opt-in on file types

--- home/vi/my-assistants.lua
+++ home/vi/my-assistants.lua
@@ -1,4 +1,14 @@
 function SetupAssistants()
     vim.cmd.packadd("copilot.vim")
-    vim.g.copilot_filetypes = {markdown = false, text = false}
+    vim.g.copilot_filetypes = {
+        ["*"] = false,
+        ["dockerfile"] = true,
+        ["haskell"] = true,
+        ["javascript"] = true,
+        ["nix"] = true,
+        ["python"] = true,
+        ["scala"] = true,
+        ["sh"] = true,
+        ["typescript"] = true
+    }
 end
