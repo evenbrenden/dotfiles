a274dbf109c2d4a52ee2c8c81053730f1864889f
Even Brenden <evenbrenden@users.noreply.github.com>
2025-04-11 13:18:20 +0200

    Switch VirtualBox with Distrobox

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,14 +1,7 @@
 { pkgs, username }:
 
 {
-  virtualisation = {
-    docker.enable = true;
-    virtualbox.host = {
-      enable = false;
-      # https://discourse.nixos.org/t/virtualbox-keeps-getting-rebuilt/6612
-      enableExtensionPack = true;
-    };
-  };
-
-  users.users.${username}.extraGroups = [ "docker" "vboxusers" ];
+  environment.systemPackages = [ pkgs.distrobox ];
+  users.users.${username}.extraGroups = [ "docker" ];
+  virtualisation.docker.enable = true;
 }
