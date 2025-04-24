ea70ea27987f49f7ffcb28b478ec20d7a816ca64
Even Brenden <evenbrenden@users.noreply.github.com>
2025-04-24 10:10:30 +0200

    copilot.vim: Enable for c, cpp and lua

--- home/vi/my-assistants.lua
+++ home/vi/my-assistants.lua
@@ -2,9 +2,12 @@ function SetupAssistants()
     vim.cmd.packadd("copilot.vim")
     vim.g.copilot_filetypes = {
         ["*"] = false,
+        ["c"] = true,
+        ["cpp"] = true,
         ["dockerfile"] = true,
         ["haskell"] = true,
         ["javascript"] = true,
+        ["lua"] = true,
         ["nix"] = true,
         ["python"] = true,
         ["scala"] = true,
