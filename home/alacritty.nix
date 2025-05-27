9c4a386ec8a860eaa7a8cfafcda638ec5edf23ab
Even Brenden <evenbrenden@users.noreply.github.com>
2025-05-27 09:00:42 +0200

    Take alacritty-theme from Nixpkgs

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -1,13 +1,6 @@
 { pkgs, ... }:
 
-let
-  alacritty-theme = pkgs.fetchFromGitHub {
-    owner = "alacritty";
-    repo = "alacritty-theme";
-    rev = "94e1dc0b9511969a426208fbba24bd7448493785";
-    sha256 = "sha256-bPup3AKFGVuUC8CzVhWJPKphHdx0GAc62GxWsUWQ7Xk=";
-  };
-in {
+{
   programs.alacritty = {
     enable = true;
     settings = {
@@ -30,7 +23,7 @@ in {
           style = "Bold Oblique";
         };
       };
-      general.import = [ "${alacritty-theme}/themes/alabaster.toml" ];
+      general.import = [ "${pkgs.alacritty-theme}/alabaster.toml" ];
     };
   };
 }
