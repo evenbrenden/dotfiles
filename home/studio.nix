2a81463b74ec057dfb6101dee5fcb54074f1011d
Even Brenden <evenbrenden@users.noreply.github.com>
2025-06-28 20:09:06 +0200

    Restructure studio
    Move things under ~/studio to ~/.local/share.
    
    Symlink LV2, VST and VST3 plugins to ~/.lv2, ~/.vst and ~/.vst3, respectively.
    
    Unset LV2_PATH, VST_PATH and VST3_PATH.

--- home/studio.nix
+++ home/studio.nix
@@ -1,68 +1,61 @@
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   home = {
     file = {
-      "studio/ir/lexicon-lxp-1-impulse-responses".source = pkgs.fetchFromGitea {
+      ".local/share/impulse-responses/lexicon-lxp-1-impulse-responses".source = pkgs.fetchFromGitea {
         domain = "codeberg.org";
         owner = "evenbrenden";
         repo = "lexicon-lxp-1-impulse-responses";
         rev = "61d7c66813b9d4305297b0a9b7c62beacd4a9dac";
         hash = "sha256-IvSuKYpcdYK8/9AHM5O88ATY3vkiUEczIWZgT0QCIhU=";
       };
-      "studio/midi/instant-midi-drum-patterns".source = pkgs.fetchzip {
+      ".local/share/midi/instant-midi-drum-patterns".source = pkgs.fetchzip {
         url = "https://web.archive.org/web/20190305092857/http://www.fivepinpress.com/InstantDrumPatterns.zip";
         sha256 = "sha256-mklEKxQ5DlRzuvw4QvR20SRp/zGGOUadClk1keBqdy8=";
       };
-      "studio/sfz/ac-upright".source = fetchGit {
+      ".local/share/sfz/ac-upright".source = fetchGit {
         url = "git@github.com:evenbrenden/ac-upright.git";
         rev = "907ee4f1dc66ee1375a7bcd7fd47f58ab2105f62";
       };
-      "studio/sfz/dsmolken-double-bass".source = pkgs.fetchFromGitHub {
+      ".local/share/sfz/dsmolken-double-bass".source = pkgs.fetchFromGitHub {
         owner = "sfzinstruments";
         repo = "dsmolken.double-bass";
         rev = "v1.001";
         sha256 = "sha256-KC/uqQhr47xyalGbTuFaw63vhNZssssWeuwl9leD8nc=";
       };
-      "studio/sfz/fretls-dry".source = fetchGit {
+      ".local/share/sfz/fretls-dry".source = fetchGit {
         url = "git@github.com:evenbrenden/fretls-dry.git";
         rev = "cf80dd097a587668494877b20f409729f629c4f6";
       };
-      "studio/sfz/jsteeldrum".source = pkgs.fetchFromGitHub {
+      ".local/share/sfz/jsteeldrum".source = pkgs.fetchFromGitHub {
         owner = "sfzinstruments";
         repo = "jlearman.SteelDrum";
         rev = "e429428dd65dc645e4c9b1f134da4d2e40c400c6";
         sha256 = "sha256-zLLskiJF8D2tR5gN2Yok4c2jFLUm5SZwEODaTtxRXmo=";
       };
-      "studio/sfz/virtuosity-drums".source = pkgs.fetchFromGitHub {
+      ".local/share/sfz/virtuosity-drums".source = pkgs.fetchFromGitHub {
         owner = "sfzinstruments";
         repo = "virtuosity_drums";
         rev = "v0.924";
         sha256 = "sha256-kedn/auowOdejv0CE97Xvc3t7tkaIqnYEk0KtNO+Md4=";
       };
-      "studio/sfz/wet-fretls".source = fetchGit {
+      ".local/share/sfz/wet-fretls".source = fetchGit {
         url = "git@github.com:evenbrenden/wet-fretls.git";
         rev = "bd5cb6659e4f7a917c34d7695bf9156090a6ead1";
       };
-      "studio/sfz/yamaha-tx81z-lately-bass".source = pkgs.fetchzip {
+      ".local/share/sfz/yamaha-tx81z-lately-bass".source = pkgs.fetchzip {
         url = "http://www.tiltshiftgallery.se/audio/yamaha_tx81z_lately_bass.zip";
         sha256 = "sha256-KViSOLl3+M1VYWE+i2Y38jJK8SuNS56eHhKb8wkwGTA=";
       };
-      "studio/soundfonts/FluidR3_GM2-2.sf2".source = "${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2";
+      ".local/share/soundfonts/FluidR3_GM2-2.sf2".source = "${pkgs.soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2";
+      ".lv2/carla.lv2".source = "${pkgs.carla}/lib/lv2/carla.lv2";
+      ".lv2/sfizz.lv2".source = "${pkgs.sfizz}/lib/lv2/sfizz.lv2";
+      ".vst/carla.vst".source = "${pkgs.carla}/lib/vst/carla.vst";
+      ".vst3/sfizz.vst3".source = "${pkgs.sfizz}/lib/vst3/sfizz.vst3";
     };
     packages = [ pkgs.carla pkgs.reaper pkgs.sfizz ];
-    sessionVariables = {
-      LV2_PATH = lib.strings.concatStringsSep ":" [ "${pkgs.carla}/lib/lv2" "${pkgs.sfizz}/lib/lv2" ];
-      UHE_RT_PRIO = 64; # For u-he plugins
-      VST_PATH = builtins.concatStringsSep ":" [
-        "$HOME/.vst" # For u-he plugins
-        "${pkgs.carla}/lib/vst"
-      ];
-      VST3_PATH = builtins.concatStringsSep ":" [
-        "$HOME/.vst3" # For u-he plugins
-        "${pkgs.sfizz}/lib/vst3"
-      ];
-    };
+    sessionVariables.UHE_RT_PRIO = 64;
   };
 
   xdg.desktopEntries.ft2-clone = {
