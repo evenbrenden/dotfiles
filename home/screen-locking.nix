9bc208cd2c7e7e56dbbffa1966b402c9b41e6702
Even Brenden <evenbrenden@users.noreply.github.com>
2025-07-08 08:08:12 +0200

    Remove xidlehook service
    The Home Manager screen saver module uses the X screen saver for
    timeouts, but I wanted to use xidlehook, so I tried disabling it. That
    didn't work. Using the screen saver module as intended instead.
    
    This also means that the screen saver wasn't disabled in full screen
    mode. That's fine, I don't think I want that anymore anyway.

--- home/screen-locking.nix
+++ home/screen-locking.nix
@@ -3,44 +3,35 @@
 with pkgs.lib;
 
 {
-  services = {
-    screen-locker = {
-      enable = true;
-      lockCmd = let
-        # https://github.com/google/xsecurelock/issues/186
-        # https://github.com/google/xsecurelock/issues/182
-        xsecurelock-command = pkgs.writeShellApplication {
-          name = "xsecurelock-command";
-          runtimeInputs = [ pkgs.refresh-display pkgs.xsecurelock ];
-          text = ''
-            XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 \
-            XSECURELOCK_KEY_F7_COMMAND=refresh-display \
-            XSECURELOCK_KEY_XF86Display_COMMAND=refresh-display \
-            xsecurelock
-          '';
-        };
-      in "${getExe xsecurelock-command}";
-      xss-lock.extraOptions = [ "--transfer-sleep-lock" ];
-    };
-    xidlehook = {
-      enable = true;
-      not-when-fullscreen = true;
-      timers = [{
-        command = "${getExe' pkgs.systemd "loginctl"} lock-session \${XDG_SESSION_ID}";
-        delay = 15 * 60;
-      }];
-    };
+  services.screen-locker = {
+    enable = true;
+    inactiveInterval = 15;
+    lockCmd = let
+      # https://github.com/google/xsecurelock/issues/186
+      # https://github.com/google/xsecurelock/issues/182
+      xsecurelock-command = pkgs.writeShellApplication {
+        name = "xsecurelock-command";
+        runtimeInputs = [ pkgs.refresh-display pkgs.xsecurelock ];
+        text = ''
+          XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 \
+          XSECURELOCK_KEY_F7_COMMAND=refresh-display \
+          XSECURELOCK_KEY_XF86Display_COMMAND=refresh-display \
+          xsecurelock
+        '';
+      };
+    in "${getExe xsecurelock-command}";
+    xss-lock.extraOptions = [ "--transfer-sleep-lock" ];
   };
 
   systemd.user.services.disable-xset-s-and-dpms = {
     Unit = {
-      Description = "Disable X Screen Saver and DPMS";
+      Description = "Disable DPMS";
       After = [ "xss-lock.service" ];
     };
     Install.WantedBy = [ "xss-lock.service" ];
     Service = {
       Type = "oneshot";
-      ExecStart = "${getExe pkgs.bash} -c '${getExe pkgs.xorg.xset} s off -dpms'";
+      ExecStart = "${getExe pkgs.bash} -c '${getExe pkgs.xorg.xset} -dpms'";
     };
   };
 }
