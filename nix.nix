48c227dbb32449f5f0f3fbe9b8609b8f8bf7d0ff
Even Brenden <evenbrenden@users.noreply.github.com>
2025-08-06 13:34:00 +0200

    Remove nix.conf settings for haskell.nix

--- nix.nix
+++ nix.nix
@@ -10,10 +10,5 @@
     nixPath = [ "nixpkgs=${nixpkgs-stable}" ];
     package = pkgs.nixVersions.stable;
     registry.nixpkgs.flake = nixpkgs-stable;
-    # haskell.nix
-    settings = {
-      trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
-      substituters = [ "https://cache.iog.io" ];
-    };
   };
 }
