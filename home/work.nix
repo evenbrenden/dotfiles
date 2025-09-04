8b739d050e918705b78453b8362405df04a68f2b
Even Brenden <evenbrenden@users.noreply.github.com>
2025-09-04 08:44:35 +0200

    Work: Add google-cloud-sdk

--- home/work.nix
+++ home/work.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ roomeqwizard usbutils ];
+  home.packages = with pkgs; [ google-cloud-sdk roomeqwizard usbutils ];
 
   programs.ssh.extraConfig = builtins.readFile "${pkgs.huddly}/ssh/icelocal";
 }
