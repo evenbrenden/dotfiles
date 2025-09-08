a437f9feb59e4ea2610c7cae980d4eb946a31192
Even Brenden <evenbrenden@users.noreply.github.com>
2025-09-08 13:05:16 +0200

    NixOS work: VPN only

--- nixos/work.nix
+++ nixos/work.nix
@@ -1,38 +1,9 @@
-{ pkgs, username, ... }:
+{ username, ... }:
 
 {
-  networking.firewall.allowedUDPPorts = [
-    42105 # falconpyclient
-    50124 # falconpyclient
-  ];
-
-  programs.nix-ld.enable = true;
-
-  services = {
-    avahi = {
-      enable = true;
-      openFirewall = true;
-      nssmdns4 = true;
-    };
-    openvpn.servers.work = {
-      autoStart = false;
-      config = "config /home/${username}/openvpn/work.ovpn";
-      updateResolvConf = true;
-    };
-    udev.packages = let
-      huddly-udev-rules = pkgs.stdenv.mkDerivation {
-        name = "huddly-udev-rules";
-        src = pkgs.huddly;
-        installPhase = ''
-          mkdir -p $out/etc/udev/rules.d
-          cp $src/udev/* $out/etc/udev/rules.d/
-        '';
-      };
-    in [ huddly-udev-rules ];
-  };
-
-  systemd.tmpfiles = {
-    rules = [ "L+ /bin/bash - - - - /run/current-system/sw/bin/bash" ];
-    settings."keep-idl-cc-files"."/tmp/*.idl.cc".X.age = "-";
+  services.openvpn.servers.work = {
+    autoStart = false;
+    config = "config /home/${username}/openvpn/work.ovpn";
+    updateResolvConf = true;
   };
 }
