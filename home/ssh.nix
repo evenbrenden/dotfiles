d999e2ccf5b8f7cd1f2838f6c32a8c888d2ddf9f
Even Brenden <evenbrenden@users.noreply.github.com>
2025-09-13 21:45:32 +0200

    SSH: Set defaults

--- home/ssh.nix
+++ home/ssh.nix
@@ -1,13 +1,18 @@
 { config, ... }:
 
 {
-  # https://github.com/nix-community/home-manager/pull/7737
+  # https://github.com/nix-community/home-manager/pull/7655
   programs.ssh = {
     enable = true;
+    addKeysToAgent = "no";
+    controlMaster = "no";
+    controlPersist = "no";
+    forwardAgent = false;
+    hashKnownHosts = false;
     matchBlocks = {
-      "*".sendEnv = [ "COLORTERM" ];
       "codeberg.org".identityFile = "${config.sops.secrets.codeberg-org-private-key.path}";
       "github.com".identityFile = "${config.sops.secrets.github-com-private-key.path}";
+      "*".sendEnv = [ "COLORTERM" ];
     };
   };
 
