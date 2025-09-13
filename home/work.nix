afcfb699956e0df09d3ecd12cc2fc67bc2e14632
Even Brenden <evenbrenden@users.noreply.github.com>
2025-09-13 10:54:13 +0200

    SSH: Use includes

--- home/work.nix
+++ home/work.nix
@@ -3,8 +3,5 @@
 {
   home.packages = with pkgs; [ conan google-cloud-sdk meld pre-commit roomeqwizard usbutils ];
 
-  programs.ssh.extraConfig = pkgs.lib.strings.concatStringsSep "\n" [
-    (builtins.readFile "${pkgs.huddly}/ssh/icelocal")
-    (builtins.readFile "${pkgs.huddly}/ssh/labor")
-  ];
+  programs.ssh.includes = [ "${pkgs.huddly}/ssh/icelocal" "${pkgs.huddly}/ssh/labor" ];
 }
