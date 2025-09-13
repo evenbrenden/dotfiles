9ee15da08a0ac04fc27f8a089368f98cf54e4a31
Even Brenden <evenbrenden@users.noreply.github.com>
2025-09-13 10:50:59 +0200

    Reorder

--- home/ssh.nix
+++ home/ssh.nix
@@ -4,9 +4,9 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
-      "*".sendEnv = [ "COLORTERM" ];
       "codeberg.org".identityFile = "${config.sops.secrets.codeberg-org-private-key.path}";
       "github.com".identityFile = "${config.sops.secrets.github-com-private-key.path}";
+      "*".sendEnv = [ "COLORTERM" ];
     };
   };
 
