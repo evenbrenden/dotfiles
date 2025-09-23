97e907dce473e834dcb74b355f34b1c7a200a332
Even Brenden <evenbrenden@users.noreply.github.com>
2025-09-23 08:28:29 +0200

    Add netron

--- home/work.nix
+++ home/work.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ conan google-cloud-sdk meld pre-commit roomeqwizard usbutils ];
+  home.packages = with pkgs; [ conan google-cloud-sdk meld netron pre-commit roomeqwizard usbutils ];
 
   programs.ssh.includes = [ "${pkgs.huddly}/ssh/icefish" "${pkgs.huddly}/ssh/labor" ];
 }
