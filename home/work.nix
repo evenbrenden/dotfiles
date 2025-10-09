bc320a4fea7d81c6ca311999d28102a91542f27a
Even Brenden <evenbrenden@users.noreply.github.com>
2025-10-09 09:03:53 +0200

    Work: Add libqalculate

--- home/work.nix
+++ home/work.nix
@@ -1,7 +1,17 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ conan google-cloud-sdk meld netron pre-commit roomeqwizard usbutils unstable.uv ];
+  home.packages = with pkgs; [
+    conan
+    google-cloud-sdk
+    libqalculate
+    meld
+    netron
+    pre-commit
+    roomeqwizard
+    usbutils
+    unstable.uv
+  ];
 
   programs.ssh.includes = [ "${pkgs.huddly}/ssh/icefish" "${pkgs.huddly}/ssh/labor" ];
 }
