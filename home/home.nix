32ac53c77cbdb6ecf55ae71fcabfa02c256cfe2d
Even Brenden <evenbrenden@users.noreply.github.com>
2025-11-20 19:45:32 +0100

    Update comment

--- home/home.nix
+++ home/home.nix
@@ -165,7 +165,7 @@
         name = "autorandr-postswitch";
         runtimeInputs = with pkgs; [ hsetroot systemd ];
         text = ''
-          hsetroot -solid '#7A3E9D' # tonsky Alabaster magenta
+          hsetroot -solid '#7A3E9D' # Dark moderate violet
           # Things that need a bump post display changes
           systemctl --user restart dunst.service
           systemctl --user restart parcellite.service
