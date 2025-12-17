93ed3c2870302d8f62628e982584df7ec6d62e16
Even Brenden <evenbrenden@users.noreply.github.com>
2025-12-17 16:17:13 +0100

    ssh: Remove nonexistent include

--- home/work.nix
+++ home/work.nix
@@ -3,5 +3,5 @@
 {
   home.packages = with pkgs; [ codex huddly-cli libqalculate meld netron networkmanagerapplet roomeqwizard usbutils ];
 
-  programs.ssh.includes = [ "${pkgs.huddly}/ssh/icefish" "${pkgs.huddly}/ssh/huddly" ];
+  programs.ssh.includes = [ "${pkgs.huddly}/ssh/huddly" ];
 }
