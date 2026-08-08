5b216accb774c7b7d7e3643fb49e90243bf9fdd9
Even Brenden <evenbrenden@users.noreply.github.com>
2026-08-08 20:27:31 +0200

    Revert "Underscore unused parameter"
    This reverts commit 487910f1f0f741b8de6bd881b9a0efec896cdc8c.

--- nixpkgs/nixpkgs.nix
+++ nixpkgs/nixpkgs.nix
@@ -14,7 +14,7 @@ in
   nixpkgs = {
     inherit config;
     overlays = [
-      (_: prev: {
+      (final: prev: {
         git-replace = import ./git-replace.nix { pkgs = prev; };
         huddly = import ./huddly.nix;
         huddly-cli = import ./huddly-cli.nix { pkgs = prev; };
