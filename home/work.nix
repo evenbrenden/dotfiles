0da15854ed68d6c25bb85c9f19313a2d61d928e6
Even Brenden <evenbrenden@users.noreply.github.com>
2026-09-02 08:16:54 +0200

    work: Inline hubba config

--- home/work.nix
+++ home/work.nix
@@ -15,9 +15,17 @@
     usbutils
   ];
 
-  programs.ssh.includes = [
-    "${pkgs.huddly}/ssh/hubba"
-    "${pkgs.huddly}/ssh/ssh_ci_config"
-    "${pkgs.huddly}/ssh/ssh_config"
-  ];
+  programs.ssh = {
+    includes = [
+      "${pkgs.huddly}/ssh/ssh_ci_config"
+      "${pkgs.huddly}/ssh/ssh_config"
+    ];
+    settings = {
+      "hubba" = {
+        AddKeysToAgent = "yes";
+        IdentityFile = "~/.ssh/even.brenden";
+        Hostname = "10.100.99.169";
+      };
+    };
+  };
 }
